135725654c089292b87ba81bd4ed7c4d4e09ab39
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-03 23:02:37 -0400

    Desenvolvimento da aplicação APP

--- app/componentes/camera.php
+++ app/componentes/camera.php
@@ -1,10 +1,10 @@
 <style>
     #preview{
+        position:fixed;
+        top:0;
+        left:0;
+        bottom:0;
         width:100%;
-        height:300px;
-        margin-top:70px;
-        border:solid 1px red;
-        z-index:999;
     }
 </style>
 <div class="col">
