eccb32c4f4585f13bd81e4fc92648f94122cf752
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-04 08:31:33 -0400

    Desenvolvimento da aplicação APP

--- app/componentes/camera.php
+++ app/componentes/camera.php
@@ -2,40 +2,18 @@
     include("../../lib/includes.php");
 ?>
 <style>
-    #preview<?=$md5?>{
+    #videoCaptura{
         position:fixed;
         top:0px;
         left:0px;
         width:100%;
         height: 100%;
         border:solid 2px red;
+        margin:0;
+        padding:0;
     }
 </style>
-<div class="col">
-    <video id="preview<?=$md5?>"></video>
+    <iframe id="videoCaptura" src="lib/vendor/camera/camera.html"></iframe>
     <script>
 
-
-        let scanner<?=$md5?> = new Instascan.Scanner(
-            {
-                video: document.getElementById('preview<?=$md5?>')
-            }
-        );
-        scanner<?=$md5?>.addListener('scan', function(content) {
-            alert('Escaneou o conteudo: ' + content);
-            window.open(content, "_blank");
-        });
-        Instascan.Camera.getCameras().then(cameras =>
-        {
-            if(cameras.length > 0){
-                scanner<?=$md5?>.start(cameras[1]);
-                console.error(cameras);
-            } else {
-                console.error("Não existe câmera no dispositivo!");
-            }
-        });
-
-
     </script>
-
-</div>
