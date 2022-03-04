b0d3e83554f682057deaca33653f00816e248816
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-04 08:47:44 -0400

    Desenvolvimento da aplicação APP

--- app/componentes/camera.php
+++ app/componentes/camera.php
@@ -12,8 +12,20 @@
         margin:0;
         padding:0;
     }
+    #DadosCaptura{
+        position:fixed;
+        bottom:10px;
+        left:0px;
+        width:100%;
+        padding:10px;
+        color:#fff;
+        font-weight:bold;
+        text-align:center;
+        border:solid 1px green;
+    }
 </style>
     <iframe id="videoCaptura" src="../lib/vendor/camera/camera.html"></iframe>
+    <div id="DadosCaptura"></div>
     <script>
 
     </script>
