2c6de029a37622e0754c9cbf012bb7342061ac00
DESKTOP-SVTJC06\raicley_santana <raicleysantana1@gmail.com>
2022-03-04 23:36:39 -0400

    Modificação no terminal

--- terminal/home/comanda.php
+++ terminal/home/comanda.php
@@ -1,36 +1,53 @@
 <?php
-    include("../../lib/includes.php");
+include("../../lib/includes.php");
 ?>
 <style>
-    .comanda{
-        position:absolute;
-        right:0;
-        top:0;
-        bottom:0;
-        width:40%;
-        overflow:auto;
+    .comanda {
+        position: absolute;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        width: 40%;
+        overflow: auto;
     }
-    .itens<?=$md5?>{
-        margin:10px;
+
+    .itens<?=$md5?> {
+        margin: 10px;
     }
 </style>
 
 <div class="comanda">
-<?php
-    $query = "select * from categorias";
-    $result = mysqli_query($con, $query);
-    while($d = mysqli_fetch_object($result)){
-?>
-    <div class="card itens<?=$md5?>">
-        <div class="card-body">
-            <li><?=$d->categoria?></li>
+
+    <div class="row">
+        <div class="col-8">
+            <?php
+            $query = "SELECT vp.codigo AS vp_codigo FROM vendas v "
+                . "LEFT JOIN vendas_produtos vp ON vp.venda = v.codigo "
+                . "WHERE v.situacao = '0'";
+            $result = mysqli_query($con, $query);
+
+            while ($d = mysqli_fetch_object($result)):?>
+                <?= $d->vp_codigo; ?>
+            <?php endwhile; ?>
         </div>
+        <div class="col-4"></div>
     </div>
-<?php
-    }
-?>
     <div style="position:fixed; right:20px; bottom:20px;">
+        <button sair class="btn btn-danger btn-lg">SAIR</button>
         <button class="btn btn-success btn-lg">CONCLUIR COMPRA</button>
-        <button class="btn btn-danger btn-lg">SAIR</button>
     </div>
 </div>
+
+
+<script>
+    $(function () {
+        $("button[sair]").click(function () {
+            $.ajax({
+                url: "home/index.php",
+                success: function (dados) {
+                    $("#body").html(dados);
+                }
+            })
+        });
+    })
+</script>
