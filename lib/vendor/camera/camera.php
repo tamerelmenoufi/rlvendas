ee8dab734923d068c34d1840a4300788c14678df
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-04 22:40:45 -0400

    Desenvolvimento da aplicação APP

--- lib/vendor/camera/camera.php
+++ lib/vendor/camera/camera.php
@@ -38,18 +38,11 @@
             html5QrcodeScanner.clear();
         }
 
-        const formatsToSupport = [
-            Html5QrcodeSupportedFormats.QR_CODE,
-            Html5QrcodeSupportedFormats.UPC_A,
-            Html5QrcodeSupportedFormats.UPC_E,
-            Html5QrcodeSupportedFormats.UPC_EAN_EXTENSION,
-        ];
         const html5QrcodeScanner = new Html5QrcodeScanner(
         "preview",
         {
             fps: 10,
             qrbox: { width: 250, height: 250 },
-            formatsToSupport: formatsToSupport
         },
         verbose=true);
         html5QrcodeScanner.render(onScanSuccess);
