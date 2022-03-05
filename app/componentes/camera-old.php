5cc56f5318f784b0021344f8a182b4e6de1e5736
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-04 21:59:06 -0400

    Desenvolvimento da aplicação APP

---
+++ app/componentes/camera-old.php
@@ -0,0 +1,118 @@
+<?php
+    include("../../lib/includes.php");
+
+
+    $mesas = [];
+    $query = "SELECT * FROM mesas WHERE situacao = '1' AND deletado != '1'";
+    $result = mysqli_query($con, $query);
+    while($m = mysqli_fetch_object($result)){
+        $mesas[] = $m->mesa;
+    }
+?>
+<style>
+    #videoCaptura{
+        position:fixed;
+        top:0px;
+        left:0px;
+        width:100%;
+        height: 100%;
+        border:solid 2px red;
+        margin:0;
+        padding:0;
+        flex:1;
+    }
+    #DadosCaptura{
+        position:fixed;
+        bottom:10px;
+        left:0px;
+        width:100%;
+        padding:10px;
+        color:#fff;
+        font-weight:bold;
+        text-align:center;
+        border:solid 1px green;
+    }
+    #QuadroCaptura{
+        position:fixed;
+        top:50%;
+        left:50%;
+        margin-left:-150px;
+        margin-top:-150px;
+        width:300px;
+        height:300px;
+    }
+    #QuadroCaptura .opc1{
+        position:absolute;
+        left:0;
+        top:0;
+        width:25%;
+        height:25%;
+        border-top:solid 2px green;
+        border-left:solid 2px green;
+        opacity:0.5;
+    }
+    #QuadroCaptura .opc2{
+        position:absolute;
+        right:0;
+        top:0;
+        width:25%;
+        height:25%;
+        border-top:solid 2px green;
+        border-right:solid 2px green;
+        opacity:0.5;
+    }
+    #QuadroCaptura .opc3{
+        position:absolute;
+        left:0;
+        bottom:0;
+        width:25%;
+        height:25%;
+        border-bottom:solid 2px green;
+        border-left:solid 2px green;
+        opacity:0.5;
+    }
+    #QuadroCaptura .opc4{
+        position:absolute;
+        right:0;
+        bottom:0;
+        width:25%;
+        height:25%;
+        border-right:solid 2px green;
+        border-bottom:solid 2px green;
+        opacity:0.5;
+    }
+</style>
+    <iframe name="videoCaptura" id="videoCaptura" src="../lib/vendor/camera/camera.html?<?=$md5?>"></iframe>
+    <div id="DadosCaptura"></div>
+    <div id="QuadroCaptura">
+        <div class="opc1"></div>
+        <div class="opc2"></div>
+        <div class="opc3"></div>
+        <div class="opc4"></div>
+    </div>
+
+    <script>
+        function LeituraCamera(content){
+
+            m = ['<?=@implode("','",$mesas)?>'];
+
+            if(mesa && $.inArray( content, m ) != -1){
+                window.localStorage.setItem('AppMesa', content);
+
+                $.ajax({
+                    url:"home/index.php?mesa="+mesa,
+                    success:function(dados){
+                        $("#body").html(dados);
+                    }
+                });
+            }else{
+                $.alert('CÓDIGO <b>'+content+'</b> BLOQUEADO, EM USO OU NÃO REGISTRADO NO SISTEMA!');
+            }
+
+
+            //document.getElementById('DadosCaptura').innerHTML = 'Adicionado pela função: ' + content;
+            //AppMesa = window.localStorage.getItem('AppMesa');
+            //window.localStorage.setItem('AppMesa', content);
+            //var valor = window.parent.videoCaptura.document.getElementById('campoTeste').value;
+        }
+    </script>
--- app/componentes/camera.php
+++ app/componentes/camera.php
@@ -21,99 +21,17 @@
         padding:0;
         flex:1;
     }
-    #DadosCaptura{
-        position:fixed;
-        bottom:10px;
-        left:0px;
-        width:100%;
-        padding:10px;
-        color:#fff;
-        font-weight:bold;
-        text-align:center;
-        border:solid 1px green;
-    }
-    #QuadroCaptura{
-        position:fixed;
-        top:50%;
-        left:50%;
-        margin-left:-150px;
-        margin-top:-150px;
-        width:300px;
-        height:300px;
-    }
-    #QuadroCaptura .opc1{
-        position:absolute;
-        left:0;
-        top:0;
-        width:25%;
-        height:25%;
-        border-top:solid 2px green;
-        border-left:solid 2px green;
-        opacity:0.5;
-    }
-    #QuadroCaptura .opc2{
-        position:absolute;
-        right:0;
-        top:0;
-        width:25%;
-        height:25%;
-        border-top:solid 2px green;
-        border-right:solid 2px green;
-        opacity:0.5;
-    }
-    #QuadroCaptura .opc3{
-        position:absolute;
-        left:0;
-        bottom:0;
-        width:25%;
-        height:25%;
-        border-bottom:solid 2px green;
-        border-left:solid 2px green;
-        opacity:0.5;
-    }
-    #QuadroCaptura .opc4{
-        position:absolute;
-        right:0;
-        bottom:0;
-        width:25%;
-        height:25%;
-        border-right:solid 2px green;
-        border-bottom:solid 2px green;
-        opacity:0.5;
-    }
 </style>
-    <iframe name="videoCaptura" id="videoCaptura" src="../lib/vendor/camera/camera.html?<?=$md5?>"></iframe>
-    <div id="DadosCaptura"></div>
-    <div id="QuadroCaptura">
-        <div class="opc1"></div>
-        <div class="opc2"></div>
-        <div class="opc3"></div>
-        <div class="opc4"></div>
-    </div>
+    <div id="videoCaptura"></div>
 
     <script>
-        function LeituraCamera(content){
-
-            m = ['<?=@implode("','",$mesas)?>'];
-
-
-            if(mesa && $.inArray( content, m ) != -1){
-                window.localStorage.setItem('AppMesa', content);
-
-                $.ajax({
-                    url:"home/index.php?mesa="+mesa,
-                    success:function(dados){
-                        $("#body").html(dados);
-                    }
-                });
-            }else{
-                $.alert('CÓDIGO <b>'+content+'</b> BLOQUEADO, EM USO OU NÃO REGISTRADO NO SISTEMA!');
-            }
-
-
-            //document.getElementById('DadosCaptura').innerHTML = 'Adicionado pela função: ' + content;
-            //AppMesa = window.localStorage.getItem('AppMesa');
-            //window.localStorage.setItem('AppMesa', content);
-            //var valor = window.parent.videoCaptura.document.getElementById('campoTeste').value;
+        function onScanSuccess(decodedText, decodedResult) {
+            // Handle on success condition with the decoded text or result.
+            console.log(`Scan result: ${decodedText}`, decodedResult);
+            $.alert(`Scan result: ${decodedText}`, decodedResult);
         }
+
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "videoCaptura", { fps: 10, qrbox: 250 });
+        html5QrcodeScanner.render(onScanSuccess);
     </script>
