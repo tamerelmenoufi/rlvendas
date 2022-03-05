fdf3a4abb62d4b9ccf8fed8c1578840f90a2435e
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-05 14:45:14 -0400

    Desenvolvimento da aplicação APP

--- app/componentes/ms_rodape.php
+++ app/componentes/ms_rodape.php
@@ -4,6 +4,21 @@
 
 <div class="row">
     <div class="col"><i class="fa-solid fa-circle-user"></i><p>Cliente</p></div>
-    <div class="col"><i class="fa-solid fa-bell-concierge"></i><p>Pedido <?=$_SESSION['AppMesa']?></p></div>
+    <div class="col IconPedido"><i class="fa-solid fa-bell-concierge"></i><p>Pedido <?=$_SESSION['AppMesa']?></p></div>
     <div class="col"><i class="fa-solid fa-circle-dollar-to-slot"></i><p>Pagar</p></div>
 </div>
+<script>
+    $(function(){
+
+        $(".IconPedido").click(function(){
+            $.ajax({
+                url:"componentes/ms_popup.php",
+                type:"POST",
+                data:{
+                    local:""
+                }
+            });
+        });
+
+    })
+</script>
