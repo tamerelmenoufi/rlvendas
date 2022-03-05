d3618de6b042b6f64b12b717f52c4f207a9345ec
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-05 13:58:30 -0400

    Desenvolvimento da aplicação APP

--- app/componentes/camera.php
+++ app/componentes/camera.php
@@ -20,7 +20,15 @@
         flex:1;
     }
 </style>
-    <iframe name="videoCaptura" id="videoCaptura" src="../lib/vendor/camera/camera.php?<?=$md5?>"></iframe>
+    <iframe
+            name="videoCaptura"
+            id="videoCaptura"
+            src="../lib/vendor/camera/camera.php?<?=$md5?>"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+    >
+    </iframe>
 
     <script>
         function LeituraCamera(content){
