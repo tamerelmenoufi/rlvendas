b0281fbd5338213f422539bf1a6db7f78c7e24ce
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-06 13:33:42 -0400

    Desenvolvimento da aplicação APP

--- app/src/produtos/produtos.php
+++ app/src/produtos/produtos.php
@@ -41,7 +41,7 @@
 
 
     .tooltip.bs-tooltip-left {
-        border-left-color: #ff0000 !important;
+        background-color: #ff0000 !important;
     }
     .tooltip.bs-tooltip-left .arrow:before {
         border-left-color: #ff0000 !important;
