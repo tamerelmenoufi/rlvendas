7f3ee93a0ac2f271128bfcc319f4cedccb794f48
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-06 16:13:16 -0400

    Desenvolvimento da aplicação APP

--- app/js/app.js
+++ app/js/app.js
@@ -96,7 +96,7 @@ RenovaSessao = () =>{
             }
         });
     }else{
-        alert("Pedido:" + AppPedido + "\nVenda:" + AppVenda + "\nCliente:" + AppCliente + "\n");
+        alert("Pedido:" + AppPedido + " Venda:" + AppVenda + " Cliente:" + AppCliente );
         //alert("Pedido:" + AppPedido);
         //alert("Cliente:" + AppCliente);
         //alert("Venda:" + AppVenda);
