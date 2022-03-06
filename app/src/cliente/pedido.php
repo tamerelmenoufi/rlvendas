5912603b5e14fdb95c4b73348ffd3f967adbaf5d
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-06 01:07:37 -0400

    Desenvolvimento da aplicação APP

--- app/src/cliente/pedido.php
+++ app/src/cliente/pedido.php
@@ -48,13 +48,13 @@
                 //print_r($pedido)
 
         ?>
-        <div class="card mb-3" style="paddin-bottom:60px;">
+        <div class="card mb-3" style="padding-bottom:60px;">
             <div class="card-body">
             <h5 class="card-title" style="paddig:0; margin:0; font-size:14px; font-weight:bold;">
                 <?=$pedido->categoria->descricao?>
                 - <?=$pedido->medida->descricao?>
             </h5>
-            <p class="card-text" style="paddig:0; margin:0;">
+            <p class="card-text" style="padding:0; margin:0;">
                 <?php
                     $ListaPedido = [];
                     for($i=0; $i < count($pedido->produtos); $i++){
@@ -62,7 +62,7 @@
                     }
                 ?>
             </p>
-            <p class="card-text" style="paddig:0; margin:0;">
+            <p class="card-text" style="padding:0; margin:0;">
                 <small class="text-muted">
                 <?php
                     if($ListaPedido) echo implode(', ', $ListaPedido);
