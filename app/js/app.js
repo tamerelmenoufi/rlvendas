5fb0bd22d8b6811717cba9eabf958eb918aabed9
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-06 16:14:46 -0400

    Desenvolvimento da aplicação APP

--- app/js/app.js
+++ app/js/app.js
@@ -75,9 +75,9 @@ PageClose = () => {
 
 RenovaSessao = () =>{
 
-    // var AppPedido = document.localStorage.getItem('AppPedido');
-    // var Appvenda = document.localStorage.getItem('Appvenda');
-    // var Appcliente = document.localStorage.getItem('AppCliente');
+    var AppPedido = document.localStorage.getItem('AppPedido');
+    var Appvenda = document.localStorage.getItem('Appvenda');
+    var Appcliente = document.localStorage.getItem('AppCliente');
 
     if(AppPedido && Appvenda && Appcliente){
         $.ajax({
