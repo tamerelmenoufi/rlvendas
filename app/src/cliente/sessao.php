d1d16ab2e8b9227697bb7df7efddb3a243b1ec75
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-06 15:11:21 -0400

    Desenvolvimento da aplicação APP

--- app/src/cliente/sessao.php
+++ app/src/cliente/sessao.php
@@ -3,4 +3,6 @@
     if($_POST['AppCliente']) $_SESSION['AppCliente'];
     if($_POST['AppVenda']) $_SESSION['AppVenda'];
     if($_POST['AppPedido']) $_SESSION['AppPedido'];
+
+    file_put_contents(date("YmdHis").".txt", "Cliente:{$_SESSION['AppCliente']}\nVenda:{$_SESSION['AppVenda']}\nPedido:{$_SESSION['AppPedido']}\n");
 ?>
