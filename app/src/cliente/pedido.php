eb1fad5c3cb1c89a5782e04ac58c57ae07e8e56d
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-06 01:59:36 -0400

    Desenvolvimento da aplicação APP

--- app/src/cliente/pedido.php
+++ app/src/cliente/pedido.php
@@ -98,7 +98,9 @@
                     ?>
                     </small>
                 </p>
-
+                <p class="card-text" style="padding:0; margin:0; text-align:right">
+                    R$ <?= number_format($d->valor_unitario, 2, ',', '.') ?>
+                </p>
                 <div style="position:absolute; bottom:0px; left:0px; width:100%;">
 
                         <button
