6db7b918785e775a0ae8f4a911b6854fb7ae26ec
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-07 21:32:42 -0400

    Removendo Obrigatoriedade nas datas das agendas

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -48,19 +48,28 @@
             <div class="card bg-light mb-3">
                 <div class="card-header">Dados da Compra</div>
                 <div class="card-body">
-                    <h5 class="col-6 card-title">
-                        <small>Pedido</small>
-                        <div style="font-size:18px; color:blue;"><?=$_SESSION['AppPedido']?></div>
-                    </h5>
-                    <h5 class="col-6 card-title">
-                        <small>Valor</small>
-                        <div style="font-size:20px; color:green;">R$ <?=number_format($d->total,2,',','.')?></div>
-                    </h5>
-                    <h5 class="col-12 card-title">
-                        <small>Cliente</small>
-                        <div><?="{$d->nome} {$d->telefone}"?></div>
-                    </h5>
-
+                    <div class="row">
+                        <div class="col-6">
+                            <h5 class="card-title">
+                                <small>Pedido</small>
+                                <div style="font-size:18px; color:blue;"><?=$_SESSION['AppPedido']?></div>
+                            </h5>
+                        </div>
+                        <div class="col-6">
+                            <h5 class="card-title">
+                                <small>Valor</small>
+                                <div style="font-size:20px; color:green;">R$ <?=number_format($d->total,2,',','.')?></div>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <h5 class="card-title">
+                                <small>Cliente</small>
+                                <div><?="{$d->nome} {$d->telefone}"?></div>
+                            </h5>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
