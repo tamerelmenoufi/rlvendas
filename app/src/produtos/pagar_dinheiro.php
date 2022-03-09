88cb995b02e7ff9f188e1464225afc68861d5a44
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-08 22:12:39 -0400

    Removendo Obrigatoriedade nas datas das agendas

--- app/src/produtos/pagar_dinheiro.php
+++ app/src/produtos/pagar_dinheiro.php
@@ -47,9 +47,11 @@
     <div class="row">
             <div class="col-12">
                 <div class="card text-white bg-info mb-3" style="padding:20px;">
+                    <p style="text-align:center">Efetue o pagamento diretamente no caixa ou solicite a comanda.</p>
                     <div style="margin-top:60px; margin-bottom:60px; text-align:center; font-size:100px;">
                         <i class="fa-solid fa-hand-holding-dollar"></i>
                     </div>
+                    <button class="btn btn-primary btn-lg btn-block">Solicitar Comanda</button>
                 </div>
             </div>
         </div>
