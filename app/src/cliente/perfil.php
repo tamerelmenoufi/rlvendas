1a2d5987dbc2072789f6abf3cbe2c1b831eb4c6a
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-10 14:49:10 -0400

    Removendo Obrigatoriedade nas datas das agendas

--- app/src/cliente/perfil.php
+++ app/src/cliente/perfil.php
@@ -25,36 +25,36 @@
                 <div class="form-row">
                     <div class="form-group col-md-6">
                     <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4">
+                    <input type="email" class="form-control form-control-lg" id="inputEmail4">
                     </div>
                     <div class="form-group col-md-6">
                     <label for="inputPassword4">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4">
+                    <input type="password" class="form-control form-control-lg" id="inputPassword4">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                    <input type="text" class="form-control form-control-lg" id="inputAddress" placeholder="1234 Main St">
                 </div>
                 <div class="form-group">
                     <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                    <input type="text" class="form-control form-control-lg" id="inputAddress2" placeholder="Apartment, studio, or floor">
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
                     <label for="inputCity">City</label>
-                    <input type="text" class="form-control" id="inputCity">
+                    <input type="text" class="form-control form-control-lg" id="inputCity">
                     </div>
                     <div class="form-group col-md-4">
                     <label for="inputState">State</label>
-                    <select id="inputState" class="form-control">
+                    <select id="inputState" class="form-control form-control-lg">
                         <option selected>Choose...</option>
                         <option>...</option>
                     </select>
                     </div>
                     <div class="form-group col-md-2">
                     <label for="inputZip">Zip</label>
-                    <input type="text" class="form-control" id="inputZip">
+                    <input type="text" class="form-control form-control-lg" id="inputZip">
                     </div>
                 </div>
                 <div class="form-group">
