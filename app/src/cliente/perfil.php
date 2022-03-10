d9650133427c4a0fa79445caf6aeb8e7319ce73d
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-10 15:03:23 -0400

    Removendo Obrigatoriedade nas datas das agendas

--- app/src/cliente/perfil.php
+++ app/src/cliente/perfil.php
@@ -19,57 +19,25 @@
 <div class="col" style="margin-bottom:60px;">
     <div class="row">
             <div class="col-12">
-
-
-                <form>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control form-control-lg" id="inputEmail4">
-                    </div>
-                    <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
-                    <input type="password" class="form-control form-control-lg" id="inputPassword4">
-                    </div>
+                <div class="form-group">
+                    <label for="nome">Nome Completo</label>
+                    <input type="text" class="form-control form-control-lg" id="nome" placeholder="Seu Nome Completo">
                 </div>
                 <div class="form-group">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control form-control-lg" id="inputAddress" placeholder="1234 Main St">
-                </div>
-                <div class="form-group">
-                    <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control form-control-lg" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                    <label for="email">E-mail</label>
+                    <input type="email" class="form-control form-control-lg" id="email" placeholder="seuemail@seudominio.com">
                 </div>
                 <div class="form-row">
-                    <div class="form-group col-md-6">
-                    <label for="inputCity">City</label>
-                    <input type="text" class="form-control form-control-lg" id="inputCity">
+                    <div class="form-group col">
+                    <label for="Senha">Senha</label>
+                    <input type="text" class="form-control form-control-lg" id="Senha">
                     </div>
-                    <div class="form-group col-md-4">
-                    <label for="inputState">State</label>
-                    <select id="inputState" class="form-control form-control-lg">
-                        <option selected>Choose...</option>
-                        <option>...</option>
-                    </select>
-                    </div>
-                    <div class="form-group col-md-2">
-                    <label for="inputZip">Zip</label>
-                    <input type="text" class="form-control form-control-lg" id="inputZip">
+                    <div class="form-group col">
+                    <label for="ConfirmaSenha">Confirmar Senha</label>
+                    <input type="text" class="form-control form-control-lg" id="ConfirmaSenha">
                     </div>
                 </div>
-                <div class="form-group">
-                    <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                    <label class="form-check-label" for="gridCheck">
-                        Check me out
-                    </label>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
-                </form>
-
-
-
+                <button type="buttom" class="btn btn-primary">Salvar dados</button>
             </div>
         </div>
     </div>
