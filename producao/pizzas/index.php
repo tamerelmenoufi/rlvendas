3fa18ae7f7abeaf059a7f81ab37eba41ac9fc3ad
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-12 17:04:04 -0400

    Desenvolvendo a aplicação da produção

--- producao/pizzas/index.php
+++ producao/pizzas/index.php
@@ -1,20 +1,21 @@
 <?php
     include("../../lib/includes.php");
 ?>
+<div class="col">
+    <div class="row">
+        <div class="col">
+            Dados da cozenha (Produção de Pizzas)
+        </div>
 
-<div class="row">
-    <div class="col">
-        Dados da cozenha (Produção de Pizzas)
+    <?php
+        $query = "select * from vendas_produtos where situacao = 'p' order by data asc";
+        $result = mysqli_query($con, $query);
+
+        while($d = mysqli_fetch_object($result)){
+    ?>
+
+    <?php
+        }
+    ?>
     </div>
-
-<?php
-    $query = "select * from vendas_produtos where situacao = 'p' order by data asc";
-    $result = mysqli_query($con, $query);
-
-    while($d = mysqli_fetch_object($result)){
-?>
-
-<?php
-    }
-?>
 </div>
