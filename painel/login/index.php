35de56ef5c28aeb894da2a3330bd3ecc66240697
raicleysantana <raicleysantana1@gmail.com>
2022-03-14 19:06:47 -0400

    Ajustes no painel

--- painel/login/index.php
+++ painel/login/index.php
@@ -22,6 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo json_encode(
             ['status' => false,
                 'msg' => 'Usuário ou senha incorreto',
+                'query' => $query,
             ]);
     }
     exit();
