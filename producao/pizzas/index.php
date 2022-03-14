65b07ae6a6fdb82c83acfbb2f1bb0a1fed4c945e
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-13 21:19:11 -0400

    Desenvolvendo a aplicação da produção

--- producao/pizzas/index.php
+++ producao/pizzas/index.php
@@ -173,49 +173,51 @@
 
 
 
+    function WebSocket(){
 
+        const ws = new WebSocket("wss://websocket.yobom.com.br");
+        //const input = document.querySelector('input');
+        const output = document.querySelector('output');
 
-    const ws = new WebSocket("wss://websocket.yobom.com.br");
-    //const input = document.querySelector('input');
-    const output = document.querySelector('output');
+        ws.addEventListener('open', console.log);
+        ws.addEventListener('message', console.log);
+        ws.addEventListener('close', function(){
+            WebSocket();
+        });
 
-    ws.addEventListener('open', console.log);
-    ws.addEventListener('message', console.log);
-    ws.addEventListener('close', console.log);
+        ws.addEventListener('message', message => {
+            const dados = JSON.parse(message.data);
+            if(dados.type === 'chat'){
+                output.append('Outro: ' + dados.text, document.createElement('br'));
+                nova_linha = '<tr>'+
+                                '<td>'+
+                                '    <div class="form-group form-check">'+
+                                '        <input status cod="<?=$d->codigo?>" <?=(($d->situacao == 'i')?'checked':false)?> type="checkbox" class="form-check-input" id="<?="{$opc}{$d->codigo}"?>">'+
+                                '    </div>'+
+                                '</td>'+
+                                '<td><label class="form-check-label" for="<?="{$opc}{$d->codigo}"?>">XXX <?=$d->mesa?></label></td>'+
+                                '<td><label class="form-check-label" for="<?="{$opc}{$d->codigo}"?>"><b><?=$d->quantidade?></b></label></td>'+
+                                '<td><label class="form-check-label" for="<?="{$opc}{$d->codigo}"?>">'+
+                                '    <?=$pedido->categoria->descricao?>'+
+                                '    - <?=$pedido->medida->descricao?> (<?=$sabores?>)'+
+                                '    <p class="card-text" style="color:red;">'+
+                                '    <?= $d->produto_descricao?></p>'+
+                                '</label></td>'+
+                                '<td><button concluir cod="<?=$d->codigo?>" class="btn btn-primary btn-sm">Concluir</button></td>'+
+                                '</tr>';
+                $("table[pizzas]").append(nova_linha);
+            }
+        })
 
-    ws.addEventListener('message', message => {
-        const dados = JSON.parse(message.data);
-        if(dados.type === 'chat'){
-            output.append('Outro: ' + dados.text, document.createElement('br'));
-            nova_linha = '<tr>'+
-                            '<td>'+
-                            '    <div class="form-group form-check">'+
-                            '        <input status cod="<?=$d->codigo?>" <?=(($d->situacao == 'i')?'checked':false)?> type="checkbox" class="form-check-input" id="<?="{$opc}{$d->codigo}"?>">'+
-                            '    </div>'+
-                            '</td>'+
-                            '<td><label class="form-check-label" for="<?="{$opc}{$d->codigo}"?>">XXX <?=$d->mesa?></label></td>'+
-                            '<td><label class="form-check-label" for="<?="{$opc}{$d->codigo}"?>"><b><?=$d->quantidade?></b></label></td>'+
-                            '<td><label class="form-check-label" for="<?="{$opc}{$d->codigo}"?>">'+
-                            '    <?=$pedido->categoria->descricao?>'+
-                            '    - <?=$pedido->medida->descricao?> (<?=$sabores?>)'+
-                            '    <p class="card-text" style="color:red;">'+
-                            '    <?= $d->produto_descricao?></p>'+
-                            '</label></td>'+
-                            '<td><button concluir cod="<?=$d->codigo?>" class="btn btn-primary btn-sm">Concluir</button></td>'+
-                            '</tr>';
-            $("table[pizzas]").append(nova_linha);
-        }
-    })
+        // input.addEventListener('keypress', e => {
+        //     if(e.code === 'Enter'){
+        //         const valor = input.value;
+        //         output.append('Eu: ' + valor, document.createElement('br'));
+        //         ws.send(valor);
 
-    // input.addEventListener('keypress', e => {
-    //     if(e.code === 'Enter'){
-    //         const valor = input.value;
-    //         output.append('Eu: ' + valor, document.createElement('br'));
-    //         ws.send(valor);
-
-    //         input.value = '';
-    //     }
-    // });
-
+        //         input.value = '';
+        //     }
+        // });
+    }
 
 </script>
--- producao/swolle.php
+++ producao/swolle.php
@@ -13,30 +13,37 @@
 
 
 <script>
-    const ws = new WebSocket("wss://websocket.yobom.com.br");
-    const input = document.querySelector('input');
-    const output = document.querySelector('output');
 
-    ws.addEventListener('open', console.log);
-    ws.addEventListener('message', console.log);
-    ws.addEventListener('close', console.log);
+    function WebSocket(){
 
-    ws.addEventListener('message', message => {
-        const dados = JSON.parse(message.data);
-        if(dados.type === 'chat'){
-            output.append('Outro: ' + dados.text, document.createElement('br'));
-        }
-    })
+        const ws = new WebSocket("wss://websocket.yobom.com.br");
+        const input = document.querySelector('input');
+        const output = document.querySelector('output');
 
-    input.addEventListener('keypress', e => {
-        if(e.code === 'Enter'){
-            const valor = input.value;
-            output.append('Eu: ' + valor, document.createElement('br'));
-            ws.send(valor);
+        ws.addEventListener('open', console.log);
+        ws.addEventListener('message', console.log);
+        ws.addEventListener('close', function(){
+            WebSocket();
+        });
 
-            input.value = '';
-        }
-    });
+        ws.addEventListener('message', message => {
+            const dados = JSON.parse(message.data);
+            if(dados.type === 'chat'){
+                output.append('Outro: ' + dados.text, document.createElement('br'));
+            }
+        })
+
+        input.addEventListener('keypress', e => {
+            if(e.code === 'Enter'){
+                const valor = input.value;
+                output.append('Eu: ' + valor, document.createElement('br'));
+                ws.send(valor);
+
+                input.value = '';
+            }
+        });
+
+    }
 </script>
 
 </body>
