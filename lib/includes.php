a6725b35755511d630f85e7f6d0bb86d96d2eb36
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-17 19:33:06 -0400

    Desenvolvendo a aplicação da produção

--- lib/includes.php
+++ lib/includes.php
@@ -4,4 +4,5 @@ include "connection.php";
 include "config.php";
 include "utils.php";
 include "fn.php";
+include "rede/classes.php";
 $md5 = md5(date("YmdHis"));
---
+++ lib/vendor/rede/classes.php
@@ -0,0 +1,23 @@
+<?php
+
+
+    Class Rede {
+
+        function Pay(){
+            return 'Efetuar pagamento!';
+        }
+
+        function Capture(){
+            return 'Realizar Captura!';
+        }
+
+        function Cancel(){
+            return 'Efetuar Cancelamento 1!';
+        }
+
+        function Cancel2(){
+            return 'Efetuar Cancrlamento 2!';
+        }
+
+
+    }
