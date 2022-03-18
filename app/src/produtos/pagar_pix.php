846051f1ca431bd5e22373fa45215cb96eec5c28
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-18 11:37:01 -0400

    Adaptação das classes do pagamento com a Rede

--- app/src/produtos/pagar_pix.php
+++ app/src/produtos/pagar_pix.php
@@ -55,25 +55,29 @@
                             $pagar->PV = '19348375';
                             $pagar->TOKEN = '2b4e31d3a75b429c9ef5fdd02f2b5c59';
 
-                            $x = $pagar->Transacao('{
-                                "capture": false,
-                                "kind": "credit",
-                                "reference": "pedido3",
-                                "amount": 2099,
-                                "installments": 2,
-                                "cardholderName": "John Snow",
-                                "cardNumber": "5448280000000007",
-                                "expirationMonth": 12,
-                                "expirationYear": 2028,
-                                "securityCode": "235",
-                                "softDescriptor": "string",
-                                "subscription": false,
-                                "origin": 1,
-                                "distributorAffiliation": 0,
-                                "brandTid": "string"
-                            }');
+                            echo $pagar->capture('{amount:2099}');
 
-                            var_dump($x);
+                            // $x = $pagar->Transacao('{
+                            //     "capture": false,
+                            //     "kind": "credit",
+                            //     "reference": "pedido3",
+                            //     "amount": 2099,
+                            //     "installments": 2,
+                            //     "cardholderName": "John Snow",
+                            //     "cardNumber": "5448280000000007",
+                            //     "expirationMonth": 12,
+                            //     "expirationYear": 2028,
+                            //     "securityCode": "235",
+                            //     "softDescriptor": "string",
+                            //     "subscription": false,
+                            //     "origin": 1,
+                            //     "distributorAffiliation": 0,
+                            //     "brandTid": "string"
+                            // }');
+
+                            // var_dump($x);
+
+
 
                             // echo $pagar->Pay([
                             //     nome=>"Tamer Mohamed Elmenoufi",
--- lib/vendor/rede/classes.php
+++ lib/vendor/rede/classes.php
@@ -38,13 +38,38 @@
 
         }
 
-        public function Pay($d){
-            $retorno = "Olá {$d['nome']}, verifiquei que o seu e-mail é {$d['email']} e seu telefone é {$d['telefone']}";
-            return $retorno;
-        }
+        public function Capture($d){
 
-        public function Capture(){
-            return 'Realizar Captura!';
+            $d = json_decode($d);
+
+            $fields = "{
+                \"amount\": {$d->amount}
+              }";
+
+              return 'Amount: '.$d->amount;
+
+
+
+            //   $ch = curl_init();
+            //   curl_setopt($ch, CURLOPT_URL, "https://sandbox-erede.useredecloud.com.br/v1/transactions/{$_GET['tid']}");
+            //   curl_setopt($ch, CURLOPT_URL, $this->Ambiente($this->Ambiente)."{}");
+            //   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+            //   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            //     "Content-Type: application/json",
+            //     "Authorization: Basic ".base64_encode('19348375:2b4e31d3a75b429c9ef5fdd02f2b5c59'),
+            //   ));
+            //   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            //   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            //   curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+            //   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            //   $response = curl_exec($ch);
+            //   curl_close($ch);
+
+
+
+            //   var_dump($response);
+
+
         }
 
         public function Cancel(){
