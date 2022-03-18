741f4615cb22f8dd0b6c332d02619d0e4fb98ec3
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-18 15:36:06 -0400

    Adaptação das classes do pagamento com a Rede

--- app/src/produtos/pagar_pix.php
+++ app/src/produtos/pagar_pix.php
@@ -50,6 +50,12 @@
                     <p style="text-align:center">
                         <?php
 
+                            $Teste = [amount=>2209];
+                            echo "TESTE:<br>";
+                            echo json_encode($Teste);
+                            echo "<br><br>";
+
+
                             $pagar = new Rede;
                             $pagar->Ambiente = 'homologacao';
                             $pagar->PV = '19348375';
