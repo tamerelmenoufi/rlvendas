df0e33aa38124ffb48395490e4070d7f9ba5bb0d
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-19 13:03:26 -0400

    Adaptação das classes do pagamento com a Rede

--- lib/vendor/rede/Transacao.php
+++ lib/vendor/rede/Transacao.php
@@ -21,3 +21,21 @@ if($_POST['cardNumber']) $cardNumber = str_replace(array(' ',false),trim($_POST[
         "distributorAffiliation": '.(($_POST['distributorAffiliation'])?:'0').',
         '.(($_POST['brandTid'])?'"brandTid": '.$_POST['brandTid'].',':false).'
     }');
+
+    echo '{
+        "capture": '.(($_POST['capture'])?:'false').',
+        "kind": "'.(($_POST['kind'])?:'credit').'",
+        "reference": "'.$_POST['reference'].'",
+        "amount": '.$_POST['amount'].',
+        '.(($_POST['installments'])?'"installments": '.$_POST['installments'].',':false).'
+        "cardholderName": "'.$_POST['cardholderName'].'",
+        "cardNumber": "'.$_POST['cardNumber'].'",
+        "expirationMonth": '.$_POST['expirationMonth'].',
+        "expirationYear": '.$_POST['expirationYear'].',
+        "securityCode": "'.$_POST['securityCode'].'",
+        "softDescriptor": "'.$_POST['softDescriptor'].'",
+        "subscription": '.(($_POST['subscription'])?:'false').',
+        "origin": 1,
+        "distributorAffiliation": '.(($_POST['distributorAffiliation'])?:'0').',
+        '.(($_POST['brandTid'])?'"brandTid": '.$_POST['brandTid'].',':false).'
+    }';
