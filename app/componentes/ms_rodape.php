88d6e3ec6514e2519aca3ae07c44f22aae769550
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-23 21:40:33 -0400

    Adaptação das classes do pagamento com a Rede

--- app/componentes/ms_rodape.php
+++ app/componentes/ms_rodape.php
@@ -17,7 +17,7 @@
             AppCliente = window.localStorage.getItem('AppCliente');
             componente = $(this).attr("componente");
             local = $(this).attr("local");
-
+            alert(AppCliente);
             if(AppCliente && local == 'src/cliente/home.php'){
                 Carregando();
                 $.ajax({
