fa5e752a97dc85b35e459dfc3fccbbc8f626a665
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-04 13:50:44 -0400

    Atualizações

--- producao/pizzas/add.php
+++ producao/pizzas/add.php
@@ -3,7 +3,7 @@
 
     $cod = base64_decode($_POST['cod']);
 
-    echo $query = "select a.*, b.mesa as mesa from vendas_produtos a left join mesas b on a.mesa = b.codigo where a.codigo = ({$cod})";
+    $query = "select a.*, b.mesa as mesa from vendas_produtos a left join mesas b on a.mesa = b.codigo where a.codigo IN ({$cod})";
     $result = mysqli_query($con, $query);
 
     while($d = mysqli_fetch_object($result)){
