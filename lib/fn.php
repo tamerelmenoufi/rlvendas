90e83bf09ec9fdd0db4338346286c84859edaddf
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-08 15:43:19 -0400

    Atualizações

--- lib/fn.php
+++ lib/fn.php
@@ -86,14 +86,15 @@ function VerificarVendaApp(){
 
         //$_SESSION = [];
         // header("location:./?s=1");
-        return "<script>window.localStorage.addItem('AppPedido','{$_SESSION['AppVenda']}');</script>";
+        echo "<script>window.localStorage.addItem('AppPedido','{$_SESSION['AppVenda']}');</script>";
+        $echo = "<h1>TESTE 1<h1>";
         //exit();
     }else if(!$_SESSION['AppVenda']){
         $_SESSION['AppVenda'] = mysqli_fetch_object($r)->codigo;
-        //return "<script>window.localStorage.addItem('AppPedido','{$_SESSION['AppVenda']}');</script>";
-        return "<h1>TESTE <h1>";
+        echo "<script>window.localStorage.addItem('AppPedido','{$_SESSION['AppVenda']}');</script>";
+        $echo = "<h1>TESTE 2<h1>";
     }else{
-        return "<h1>TESTE 2<h1>" . $_SESSION['AppVenda'];
+        $echo = "<h1>TESTE 3<h1>";
     }
 
 
