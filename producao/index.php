ddf7e7f094d90c8dde4ee79c7f013a97b7131281
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-10 17:29:46 -0400

    Atualizações

--- producao/index.php
+++ producao/index.php
@@ -48,6 +48,97 @@
             animationBounce: 1.5
         }
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Socket Variable declaration
+    var mySocket;
+    const socketMessageListener = (event) => {
+
+        const dados = JSON.parse(event.data);
+        if(dados.type === 'chat'){
+            //output.append('Outro: ' + dados.text, document.createElement('br'));
+            console.log('Entrou na função');
+            console.log(dados.text);
+            $.ajax({
+                url:"pizzas/add.php",
+                type:"POST",
+                data:{
+                    cod:dados.text,
+                },
+                success:function(dados){
+                    console.log(dados);
+                    $("table[pizzas]").append(dados);
+                },
+                error:function(){
+                    console.log('algo deu errado!');
+                }
+            });
+
+            // nova_linha = '<tr>'+
+            //                 '<td>'+
+            //                 '    <div class="form-group form-check">'+
+            //                 '        <input status cod="<?=$d->codigo?>" <?=(($d->situacao == 'i')?'checked':false)?> type="checkbox" class="form-check-input" id="<?="{$opc}{$d->codigo}"?>">'+
+            //                 '    </div>'+
+            //                 '</td>'+
+            //                 '<td><label class="form-check-label" for="<?="{$opc}{$d->codigo}"?>">XXX <?=$d->mesa?></label></td>'+
+            //                 '<td><label class="form-check-label" for="<?="{$opc}{$d->codigo}"?>"><b><?=$d->quantidade?></b></label></td>'+
+            //                 '<td><label class="form-check-label" for="<?="{$opc}{$d->codigo}"?>">'+
+            //                 '    <?=$pedido->categoria->descricao?>'+
+            //                 '    - <?=$pedido->medida->descricao?> (<?=$sabores?>)'+
+            //                 '    <p class="card-text" style="color:red;">'+
+            //                 '    <?= $d->produto_descricao?></p>'+
+            //                 '</label></td>'+
+            //                 '<td><button concluir cod="<?=$d->codigo?>" class="btn btn-primary btn-sm">Concluir</button></td>'+
+            //                 '</tr>';
+
+        }
+
+
+    };
+
+    // Open
+    const socketOpenListener = (event) => {
+        console.log('Connected');
+
+        $.ajax({
+            url: "<?=$opc?>/index.php",
+            success: function (dados) {
+                $("#body").html(dados);
+            },
+            error: function () {
+                $.alert('Ocorreu um erro!');
+            }
+        });
+
+
+    };
+
+    // Closed
+    const socketCloseListener = (event) => {
+    if (mySocket) {
+        console.error('Disconnected.');
+    }
+    mySocket = new WebSocket('wss://websocket.yobom.com.br');
+    mySocket.addEventListener('open', socketOpenListener);
+    mySocket.addEventListener('message', socketMessageListener);
+    mySocket.addEventListener('close', socketCloseListener);
+    };
+    socketCloseListener();
+
+
+
+
 </script>
 </body>
 </html>
