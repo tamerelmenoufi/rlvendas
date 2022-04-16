4cf3113926754b1e8570c60fc083e3a8c8c6c584
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-16 08:30:39 -0400

    Atualizações

---
+++ lib/vendor/bee/empresas_all.php
@@ -0,0 +1,20 @@
+<?php
+
+$chave = "7ee80ecf9002e205789139ef9179b3b4c3dbe776";
+
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://integrationtest.beedelivery.com.br/api/v1/public/companies/getAll");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  "Content-Type: application/json",
+  "Authorization: {$chave}"
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
