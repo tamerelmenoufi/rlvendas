27dd5458d7b838d54ddb05ffd62b86a8039c3b38
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-16 09:01:43 -0400

    Atualizações

---
+++ lib/vendor/bee/empresa_vincular.php
@@ -0,0 +1,28 @@
+<?php
+
+$chave = "7ee80ecf9002e205789139ef9179b3b4c3dbe776";
+$externalId = 38;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://integrationtest.beedelivery.com.br/api/v1/public/companies/link");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+curl_setopt($ch, CURLOPT_POST, TRUE);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, "{
+  \"externalId\": {$externalId},
+  \"docType\": \"F\",
+  \"doc\": 60110970225
+}");
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  "Content-Type: application/json",
+  "Authorization: {$chave}"
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
