ad88d4cd45ba929fc7040f493155ca800406a95c
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-29 15:52:33 -0400

    Atualizações

--- painel/vendas/index.php
+++ painel/vendas/index.php
@@ -7,11 +7,9 @@ if($_POST['acao'] == 'NotaPdf'){
 
     $documento = GerarPDF($_POST['doc']);
 
-    $dados = json_decode($documento);
-    print_r($dados);
-    // file_put_contents($dados->name, base64_decode($dados->doc));
+    echo $dados = json_decode($documento);
 
-    // echo "{$dados['name']}";
+
     exit();
 
 }
