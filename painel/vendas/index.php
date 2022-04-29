319f983c51c05baa1ff16519f0411eb449a96dcf
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-29 16:45:49 -0400

    Atualizações

--- painel/vendas/index.php
+++ painel/vendas/index.php
@@ -219,8 +219,8 @@ $result = mysqli_query($con, $query);
                             $.alert(dados);
                             //$.alert('Dados enviados!');
 
-                            //acao = '<iframe src="http://localhost/print/print.php?pdf='+dados+'" border="0" width="0" height="0" style="opacity:0"></iframe>';
-                            //$("body").append(acao);
+                            acao = '<iframe src="http://localhost/print/print.php?pdf='+dados+'" border="0" width="0" height="0" style="opacity:0"></iframe>';
+                            $("body").append(acao);
                             //Dados de teste
 
                         }
