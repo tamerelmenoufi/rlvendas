b9b66a7b55e4cdb22710904d49eac7251a64fa1d
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-29 23:11:57 -0400

    Atualizações

--- painel/vendas/print.php
+++ painel/vendas/print.php
@@ -20,7 +20,6 @@ $retorno .= '<table width="100%" border="0" cellpadding="2" cellspacing="0">
         <tr>
             <th>Produto</th>
             <th>valor Unitário</th>
-            <th>Quantidade</th>
             <th>Total</th>
         </tr>
     </thead>
@@ -43,14 +42,11 @@ $retorno .= '<table width="100%" border="0" cellpadding="2" cellspacing="0">
 
 $retorno .= "<tr>
     <td>
-        {$pedido->categoria->descricao} - {$pedido->medida->descricao}<br>
+        {$d->quantidade} X {$pedido->categoria->descricao} - {$pedido->medida->descricao}<br>
         {$d->produto_descricao}
     </td>
     <td>
         R$ ".number_format($d->valor_unitario, 2, ',', '.')."
-    </td>
-    <td>
-        {$d->quantidade}
     </td>
     <td>
         R$ ".number_format($d->valor_total, 2, ',', '.')."
