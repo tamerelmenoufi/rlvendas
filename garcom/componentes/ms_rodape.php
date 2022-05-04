563bff9a3c5a990ad68004eb2d0096c9b926a976
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-03 20:25:52 -0400

    Atualizações

--- garcom/componentes/ms_rodape.php
+++ garcom/componentes/ms_rodape.php
@@ -8,8 +8,8 @@
 ?>
 
 <div class="row">
-    <div class="col acao" componente="ms_popup_100" local="src/mesas/home.php"><i class="fa-solid fa-circle-user"></i><p>Mesas <?=$_SESSION['AppCliente']?></p></div>
-    <div class="col acao" componente="ms_popup_100" local="src/produtos/pedido.php"><i class="fa-solid fa-bell-concierge"></i><p>Pedido <?=$m->mesa?></p></div>
+    <div class="col acao" componente="ms_popup_100" local="src/mesas/home.php"><i class="fa-solid fa-circle-user"></i><p>Mesas</p></div>
+    <div class="col acao" componente="ms_popup_100" local="src/produtos/pedido.php"><i class="fa-solid fa-bell-concierge"></i><p>Pedido</p></div>
     <div class="col acao" componente="ms_popup_100" local="src/produtos/pagar.php"><i class="fa-solid fa-circle-dollar-to-slot"></i><p>Pagar</p></div>
 </div>
 <script>
