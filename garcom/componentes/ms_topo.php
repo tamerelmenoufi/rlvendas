5d649c2437e9ccd4afe165d908a4e185306b1929
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-05 19:52:14 -0400

    Iniciando

--- garcom/componentes/ms_topo.php
+++ garcom/componentes/ms_topo.php
@@ -17,12 +17,17 @@
         margin-right:10px;
         color:#fff;
     }
+    i[sair]{
+        margin:5px;
+        color:#fff;
+        cursor:pointer;
+    }
 </style>
 <div class="row">
     <div class="col-4">
         <img class="topoImg" src="img/logo.png" />
     </div>
-    <div class="col-6">
+    <div class="col-7">
         <?php
             if($g->nome){
         ?>
@@ -36,7 +41,60 @@
             }
         ?>
     </div>
-    <div class="col-2">
-        <i sair class="fa-solid fa-exit"></i>
+    <div class="col-1">
+        <i sair class="fa-solid fa-circle-xmark"></i>
     </div>
 </div>
+
+
+<script>
+    $(function(){
+
+        $("i[sair]").click(function(){
+
+
+            $.confirm({
+                icon: "fa-solid fa-right-from-bracket",
+                content: false,
+                title: "Deseja Realmente sair do aplicativo?",
+                columnClass: "medium",
+                type: "red",
+                buttons: {
+                    'SIM': {
+                        text: "SIM",
+                        action: function () {
+
+                            window.localStorage.removeItem('AppPedido');
+                            window.localStorage.removeItem('AppCliente');
+                            window.localStorage.removeItem('AppVenda');
+                            window.localStorage.removeItem('AppGarcom');
+
+                            $.ajax({
+                                url:"src/home/index.php",
+                                type:"POST",
+                                data:{
+                                    acao:'Sair',
+                                    confirm:'1',
+                                },
+                                success:function(dados){
+                                    PageClose();
+                                    window.location.href='./?s=1';
+                                }
+                            });
+
+
+                        }
+                    },
+                    'Não': {
+                        text: "NÃO",
+                        action: function () {
+                            PageClose();
+                        }
+                    }
+                }
+            })
+
+        })
+
+    })
+</script>
