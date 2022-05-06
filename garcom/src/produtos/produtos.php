96cb56728d99f2aa9f5f2bdaf38970b2aecff3c9
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-06 11:10:14 -0400

    Iniciando

--- garcom/src/produtos/produtos.php
+++ garcom/src/produtos/produtos.php
@@ -152,7 +152,7 @@ while ($m = mysqli_fetch_array($m_r)) {
                                             medida='<?= $val["quantidade"]; ?>'
                                             valor='<?= $val['valor']; ?>'
                                             class="btn btn-outline-success btn-xs"
-                                            style="height:40px; font-size:20px; line-height: 1.2;"
+                                            style="height:auto; font-size:20px; line-height: 1.2;"
                                     >
                                         <?= $M[$key2]['descricao']; ?><br>
                                         R$ <?= number_format($val['valor'], 2, ',', '.') ?>
