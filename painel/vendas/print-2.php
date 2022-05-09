e651c9f15aef5da3407c6356cdf64785fe128f4a
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-08 23:08:38 -0400

    Atualizações

--- painel/vendas/print-2.php
+++ painel/vendas/print-2.php
@@ -50,8 +50,8 @@
     }
 
     $retorno .= "\ntxt|1|2|right|Pagar R$ ".number_format($valor_total, 2, ',', '.').""."\n\n";
-    $retorno .= "qrcode|12|12|center|https://notas.yobom.com.br/?{$p->codigo}";
-    $retorno .= "\ntxt|1|1|center|".md5($p->codigo).""."\n\n";
+    $retorno .= "qrcode|8|8|center|https://notas.yobom.com.br/?{$p->codigo}";
+    $retorno .= "\n\ntxt|1|1|center|".md5($p->codigo).""."\n\n";
     $retorno .= "txt|1|1|center|Yobom.com.br - ".date("d/m/Y H:i:s").""."\n";
 
     //$retorno = GerarPrint($retorno);
