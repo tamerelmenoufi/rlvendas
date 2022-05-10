cf2edd427445a4925acb91d33c6de86604b9c4e3
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-10 17:40:27 -0400

    Atualizações

--- producao/geral_teste/index.php
+++ producao/geral_teste/index.php
@@ -73,8 +73,8 @@
         ?>
         <div class="col-3">
 
-            <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
-                <div class="card-header"><b>MESA:</b> <?=$d->mesa?></div>
+            <div class="card text-white bg-warning mb-3">
+                <div class="card-header text-white bg-warning"><b>MESA:</b> <?=$d->mesa?></div>
                 <div class="card-body">
                     <p class="card-text">
                         <b>Quantidade: </b><?=$d->quantidade?><br>
