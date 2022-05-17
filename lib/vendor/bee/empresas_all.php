756f8a539101049219c4219ef2d9074b3e89a82d
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-17 10:27:28 -0400

    Atualizações

--- lib/vendor/bee/empresas_all.php
+++ lib/vendor/bee/empresas_all.php
@@ -17,7 +17,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 $response = curl_exec($ch);
 curl_close($ch);
 
-var_dump($response);
+echo ($response);
 
 echo "<hr>";
 foreach($response as $i => $v){
