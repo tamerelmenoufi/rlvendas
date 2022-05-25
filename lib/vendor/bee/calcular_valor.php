6bbc472d1e8d5fc03ebdf53bbadff66964c698a8
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-24 23:24:45 -0400

    Atualizações

--- lib/vendor/bee/calcular_valor.php
+++ lib/vendor/bee/calcular_valor.php
@@ -1,36 +1,46 @@
 <?php
 
-    $chave = "7ee80ecf9002e205789139ef9179b3b4c3dbe776";
+    function frete($Id){
 
-    $ch = curl_init();
+        $chave = "7ee80ecf9002e205789139ef9179b3b4c3dbe776";
 
-    curl_setopt($ch, CURLOPT_URL, "https://api.beedelivery.com.br/api/v1/public/fees/calculate");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        $ch = curl_init();
 
-    curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_URL, "https://api.beedelivery.com.br/api/v1/public/fees/calculate");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "{
-        \"vehicle\": \"M\",
-        \"needReturn\": \"N\",
-        \"origin\": {
-            \"externalId\": 37
-        },
-        \"destination\": {
-            \"type\": \"COORDS\",
-            \"address\": {
-                \"latitude\": -3.1290315,
-                \"longitude\": -60.02384969999999
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "{
+            \"vehicle\": \"M\",
+            \"needReturn\": \"N\",
+            \"origin\": {
+                \"externalId\": {$Id}
+            },
+            \"destination\": {
+                \"type\": \"COORDS\",
+                \"address\": {
+                    \"latitude\": -3.1290315,
+                    \"longitude\": -60.02384969999999
+                }
             }
-        }
-    }");
+        }");
 
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        "Content-Type: application/json",
-        "Authorization: {$chave}"
-    ));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "Authorization: {$chave}"
+        ));
 
-    $response = curl_exec($ch);
-    curl_close($ch);
+        $response = curl_exec($ch);
+        curl_close($ch);
 
-    var_dump($response);
+        var_dump($response);
+        echo "<hr>";
+    }
+
+    $l = [39,40,41,42,44,45];
+
+    for($i=0;$i<count($l);$i++){
+        frete($l[$i]);
+      }
