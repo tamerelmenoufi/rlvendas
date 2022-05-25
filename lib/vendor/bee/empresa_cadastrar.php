2d48b424be1567bfd77a0f530cce89a6d1c803fa
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-24 21:36:32 -0400

    Atualizações

--- lib/vendor/bee/empresa_cadastrar.php
+++ lib/vendor/bee/empresa_cadastrar.php
@@ -4,7 +4,8 @@ $chave = "7ee80ecf9002e205789139ef9179b3b4c3dbe776";
 $externalId = 39;
 
 
-$dados = [
+$Geral = [
+  [
     "cpfCnpj" => "26277931000125",
     "name" => "SP RESTAURANTES LTDA",
     "email" => "ger.bkparaiba@spgrupo.com",
@@ -18,10 +19,112 @@ $dados = [
     "celular" => "92984122099",
     "latitude" => "-3.0929237",
     "longitude" => "-60.0092208",
-    "externalId" => "{$externalId}"
+    "externalId" => $externalId
+  ],
+
+  [
+    "cpfCnpj" => "26277931000397",
+    "name" => "BURGER KING (Turismo)",
+    "email" => "ger.bktaruma@spgrupo.com",
+    "uf" => "AM",
+    "cidade" => "Manaus",
+    "cep" => "69041010",
+    "bairro" => "TARUMÃ",
+    "rua" => "AV DO TURISMO",
+    "numero" => "2726",
+    "telefone" => "92984125532",
+    "celular" => "92984125532",
+    "latitude" => "-3.05129",
+    "longitude" => "-60.07791",
+    "externalId" => $externalId+1
+  ],
+  [
+    "cpfCnpj" => "26277931000479",
+    "name" => "BURGER KING (Shopping Sumaúma)",
+    "email" => "ger.bksumauma@spgrupo.com",
+    "uf" => "AM",
+    "cidade" => "Manaus",
+    "cep" => "69095000",
+    "bairro" => "CIDADE NOVA",
+    "rua" => "AV NOEL NUTELS",
+    "numero" => "1762",
+    "telefone" => "92984326398",
+    "celular" => "92984326398",
+    "latitude" => "-3.0308551",
+    "longitude" => "-59.977411",
+    "externalId" => $externalId+2
+  ],
+  [
+    "cpfCnpj" => "26277931000559",
+    "name" => "BURGER KING (Studio-5)",
+    "email" => "ger.bkstudio5@spgrupo.com",
+    "uf" => "AM",
+    "cidade" => "Manaus",
+    "cep" => "69073177",
+    "bairro" => "CRESPO",
+    "rua" => "AV RODRIGO OTÁVIO",
+    "numero" => "3555",
+    "telefone" => "92984034458",
+    "celular" => "92984034458",
+    "latitude" => "-3.1247281",
+    "longitude" => "-59.9824092",
+    "externalId" => $externalId+3
+  ],
+  [
+    "cpfCnpj" => "26277931000630",
+    "name" => "BURGER KING (Via Norte)",
+    "email" => "ger.bkvianorte@spgrupo.com",
+    "uf" => "AM",
+    "cidade" => "Manaus",
+    "cep" => "69093149",
+    "bairro" => "MONTE DAS OLIVEIRA",
+    "rua" => "AV ARQUITETO JOSE HENRIQUES B RORIGUES",
+    "numero" => "3736",
+    "telefone" => "92984113947",
+    "celular" => "92984113947",
+    "latitude" => "-2.9989892",
+    "longitude" => "-60.0024618",
+    "externalId" => $externalId+4
+  ],
+  [
+    "cpfCnpj" => "26277931000800",
+    "name" => "BURGER KING (Amazonas Shopping)",
+    "email" => "ger.bkamazonas2@spgrupo.com",
+    "uf" => "AM",
+    "cidade" => "Manaus",
+    "cep" => "69050902",
+    "bairro" => "PARQUE DEZ",
+    "rua" => "AV DJALMA BATISTA",
+    "numero" => "482",
+    "telefone" => "92984388556",
+    "celular" => "92984388556",
+    "latitude" => "-3.0941363",
+    "longitude" => "-60.0226946",
+    "externalId" => $externalId+5
+  ],
+  [
+    "cpfCnpj" => "26277931000710",
+    "name" => "BURGER KING - PONTA NEGRA",
+    "email" => "ger.bkpontanegra@spgrupo.com",
+    "uf" => "AM",
+    "cidade" => "Manaus",
+    "cep" => "69037000",
+    "bairro" => "PONTA NEGRA",
+    "rua" => "AV CEL TEIXEIRA",
+    "numero" => "5705",
+    "telefone" => "92984326030",
+    "celular" => "92984326030",
+    "latitude" => "-3.0847601",
+    "longitude" => "-60.0722883",
+    "externalId" => $externalId+6
+  ],
+
 ];
 
-$fild = json_encode($dados);
+
+$dados = $Geral[1];
+
+echo $fild = json_encode($dados);
 
 
 // $dados = "{
@@ -41,11 +144,11 @@ $fild = json_encode($dados);
 //     \"externalId\": \"{$externalId}\"
 //   }";
 
-// echo json_decode($dados);
+//  echo json_decode($dados);
 
 
+exit();
 
-//   exit();
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, "https://integrationtest.beedelivery.com.br/api/v1/public/companies/new");
