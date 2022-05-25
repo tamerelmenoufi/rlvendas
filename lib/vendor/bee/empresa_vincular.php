f481c4b03b562600b70dd2eec439bbef40d8f407
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-24 22:43:49 -0400

    Atualizações

--- lib/vendor/bee/empresa_vincular.php
+++ lib/vendor/bee/empresa_vincular.php
@@ -1,28 +1,45 @@
 <?php
 
-$chave = "7ee80ecf9002e205789139ef9179b3b4c3dbe776";
-$externalId = 39;
+function Vincular($Id, $t, $c){
 
-$ch = curl_init();
+  $chave = "7ee80ecf9002e205789139ef9179b3b4c3dbe776";
+  $externalId = $Id;
 
-curl_setopt($ch, CURLOPT_URL, "https://api.beedelivery.com.br/api/v1/public/companies/link");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
+  $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_POST, TRUE);
+  curl_setopt($ch, CURLOPT_URL, "https://api.beedelivery.com.br/api/v1/public/companies/link");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, "{
-  \"externalId\": {$externalId},
-  \"docType\": \"J\",
-  \"doc\": 26277931000125
-}");
+  curl_setopt($ch, CURLOPT_POST, TRUE);
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  "Content-Type: application/json",
-  "Authorization: {$chave}"
-));
+  curl_setopt($ch, CURLOPT_POSTFIELDS, "{
+    \"externalId\": {$externalId},
+    \"docType\": \"{$t}\",
+    \"doc\": {$j}
+  }");
 
-$response = curl_exec($ch);
-curl_close($ch);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json",
+    "Authorization: {$chave}"
+  ));
 
-var_dump($response);
+  $response = curl_exec($ch);
+  curl_close($ch);
+
+  var_dump($response);
+  echo "<hr>";
+}
+
+$e = [
+  [39,'j',26277931000125],
+  [40,'j',26277931000397],
+  [41,'j',26277931000479],
+  [42,'j',26277931000559],
+  [44,'j',26277931000800],
+  [45,'j',26277931000710],
+];
+
+for($i=0;$i<count($e);$i++){
+  Vincular($e[0], $e[1], $e[2]);
+}
