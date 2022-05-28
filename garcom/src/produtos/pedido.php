3398933d454e14e910c71102e8a897bccc68782c
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-27 22:28:10 -0400

    Atualizações

--- garcom/src/produtos/pedido.php
+++ garcom/src/produtos/pedido.php
@@ -19,9 +19,9 @@
         }
         $codigos = implode(",", $codigos);
 
-        //$ordem = strtotime("now");
-        //, ordem = '{$ordem}'
-        $query = "UPDATE vendas_produtos SET situacao = 'p' WHERE codigo in ({$codigos})";
+        $ordem = strtotime("now");
+
+        $query = "UPDATE vendas_produtos SET situacao = 'p', ordem = '{$ordem}' WHERE codigo in ({$codigos})";
         if (mysqli_query($con, $query)) {
             echo json_encode([
                 "status" => "sucesso",
