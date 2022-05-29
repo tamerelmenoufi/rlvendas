38c9581d77086e648c267b6e466d56a0804ebe1a
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-29 18:18:08 -0400

    Atualizações

--- painel/atendentes/form.php
+++ painel/atendentes/form.php
@@ -45,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         echo json_encode([
             'status' => false,
-            'msg' => 'Erro ao salvar',
+            'msg' => 'Erro ao salvar '.$query,
             'codigo' => $codigo,
             'mysql_error' => mysqli_error($con),
         ]);
