34d5e3d76e824e450bf787f67daf490a80b7efa8
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-01 20:06:14 -0400

    Atualizações

--- garcom/src/produtos/pedido.php
+++ garcom/src/produtos/pedido.php
@@ -172,7 +172,7 @@
         <div class="card bg-light mb-3" style="padding-bottom:40px;">
             <div class="card-body">
                 <p Excluirproduto codigo="<?=$d->codigo?>" produto="<?=$pedido->categoria->descricao?> - <?=$pedido->medida->descricao?> <?=$sabores?>" style="position:absolute; right:-10px; top:-10px; width:auto;">
-                    <i class="fa-solid fa-circle-xmark" style="color:orange; font-size:30px; <?=$blqc?>"></i>
+                    <i class="fa-solid fa-circle-xmark" style="color:orange; font-size:30px; <?=$blqcX?>"></i>
                 <p>
                 <h5 class="card-title" style="paddig:0; margin:0; font-size:14px; font-weight:bold;">
                     <?=$pedido->categoria->descricao?>
