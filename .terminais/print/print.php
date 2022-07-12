ef3bcede83caedab2e34874001eb60199e6c71dc
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-11 22:43:07 -0400

    Atualizações

--- .terminais/print/print.php
+++ .terminais/print/print.php
@@ -18,28 +18,34 @@
 
     $result   = file_get_contents('https://yobom.com.br/rlvendas/painel/vendas/print/terminal1-c.txt');
 
-    if($result){
-	file_put_contents("docs/terminal1.txt",$result);
-	system("cat docs/terminal1.txt > /dev/usb/lp0");
+    $prov = file_get_contents("docs/provisorio.txt");
+
+    if($result and $prov){
+
+        file_put_contents("docs/provisorio.txt",md5($result));
+
+        file_put_contents("docs/terminal1.txt",$result);
+    	system("cat docs/terminal1.txt > /dev/usb/lp0");
 
 
+        $dadosParaEnviar = http_build_query(
+            array(
+                't' => 'terminal1'
+            )
+        );
 
+        $opcoes = array('http' =>
+            array(
+                'method'  => 'POST',
+                'header'  => 'Content-Type: application/x-www-form-urlencoded',
+                'content' => $dadosParaEnviar
+            )
+        );
 
-    $dadosParaEnviar = http_build_query(
-        array(
-            't' => 'terminal1'
-        )
-    );
-    $opcoes = array('http' =>
-           array(
-            'method'  => 'POST',
-            'header'  => 'Content-Type: application/x-www-form-urlencoded',
-            'content' => $dadosParaEnviar
-        )
-    );
-    $contexto = stream_context_create($opcoes);
+        $contexto = stream_context_create($opcoes);
 
-    $result   = file_get_contents('https://yobom.com.br/rlvendas/painel/vendas/print/remove.php', false, $contexto);
+        $result   = file_get_contents('https://yobom.com.br/rlvendas/painel/vendas/print/remove.php', false, $contexto);
 
+        unlink("docs/provisorio.txt");
 
     }
