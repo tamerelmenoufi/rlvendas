1964290e9fdf81a27eafcddf237b873c9cc4094b
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-31 22:56:43 -0400

    Atualizações

--- nf/emissorNF.php
+++ nf/emissorNF.php
@@ -351,7 +351,7 @@ include("config.php");
 
 			$Prod = [];
 			foreach($pedido->produtos as $ind => $prod){
-				echo $Prod[] = $prod->descricao;
+				$Prod[] = $prod->descricao;
 			}
 			$Prod = (($Prod)?implode(' ',$Prod):false);
 
@@ -361,7 +361,7 @@ include("config.php");
 
 			$codigo=$row["codigo"];
 			$quatidade = (empty($row["quantidade"])) ? "1" : $row["quantidade"];;
-			$nomeproduto=$pedido->categoria->descricao." ".$Prod." ".$pedido->medida->descricao."(".$d->produto_descricao.")"; // NOME DO PRODUTO
+			echo $nomeproduto=$pedido->categoria->descricao." ".$Prod." ".$pedido->medida->descricao."(".$d->produto_descricao.")"; // NOME DO PRODUTO
 			$ncm=$row["ncm"]; // NCM
 			$unit=(empty($row["unidade"])) ? "UN" : $row["unidade"]; // CODIGO UNIDADE
 			$origem = (empty($row["origem"])) ? "0" : $row["origem"];
