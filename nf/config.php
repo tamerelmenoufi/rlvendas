d0c5c4ab0f3a852bf5c557cc25d62b2565b15cd7
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-31 22:38:58 -0400

    Atualizações

--- nf/config.php
+++ nf/config.php
@@ -8,4 +8,4 @@ $PDO = new PDO("mysql:host=yobom.com.br;dbname=app", "root", "SenhaDoBanco");
 $endpoint = "http://nf.mohatron.com/API-NFE/api-nfe/"; // COM BARRA NO FINAL
 
 
-ini_set('display_errors', 'Off');
+ini_set('display_errors', 'On');
