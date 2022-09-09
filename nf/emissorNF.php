07f6a3982a9930176d3618d5caa0fe55be6276f6
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-08 22:19:16 -0400

    Atualizações

--- nf/emissorNF.php
+++ nf/emissorNF.php
@@ -126,9 +126,9 @@ include("config.php");
     $nota = $stmt->fetch(PDO::FETCH_ASSOC);
     $stmt = null;
 
-	// $sql = 'UPDATE configuracao set numero_proxima_nfc = (numero_proxima_nfc+1) WHERE codigo = ?';
-	// $stmt = $PDO->prepare($sql);
-	// $stmt->execute([1]);
+	$sql = 'UPDATE configuracao set numero_proxima_nfc = (numero_proxima_nfc+1) WHERE codigo = ?';
+	$stmt = $PDO->prepare($sql);
+	$stmt->execute([1]);
 
     if(empty($rowVenda)) die("Vendas nao encontrada");
 
