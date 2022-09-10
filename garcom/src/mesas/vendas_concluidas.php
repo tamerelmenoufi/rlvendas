d8cc67ec35437d810b72932246c42a23b3f68ec6
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-10 16:49:33 -0400

    Atualizações

--- garcom/src/mesas/vendas_concluidas.php
+++ garcom/src/mesas/vendas_concluidas.php
@@ -1,4 +1,8 @@
 <?php
     include("../../../lib/includes.php");
 
-    echo "Lista das vendas concluídas";
+    $query = "SELECT * FROM `vendas` where situacao = 'pagar' and deletado != '1'";
+    $result = mysqli_query($con, $query);
+    while($d = mysqli_fetch_object($resul)){
+        echo "{$d->codigo} - mesa ({$d->mesa}) valor: {$d->total}<br>";
+    }
