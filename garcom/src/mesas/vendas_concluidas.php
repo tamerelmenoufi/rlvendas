15e69799aa06f8d1d1254e43d7b19a7bb9c7e1f1
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-10 17:04:58 -0400

    Atualizações

--- garcom/src/mesas/vendas_concluidas.php
+++ garcom/src/mesas/vendas_concluidas.php
@@ -3,8 +3,8 @@
 
     $data_limite = date( "Y-m-d H:i:s", mktime(date("H"), date("i"), date("s"), date("m"), date("d") - 1, date("Y")));
 
-    echo $query = "SELECT * FROM `vendas` where ((data_finalizacao >= '{$data_limite}' and situacao = 'pago') or situacao = 'pagar') and deletado != '1'";
+    $query = "SELECT * FROM `vendas` where ((data_finalizacao >= '{$data_limite}' and situacao = 'pago') or situacao = 'pagar') and deletado != '1'";
     $result = mysqli_query($con, $query);
-    while($d = mysqli_fetch_object($resul)){
+    while($d = mysqli_fetch_object($result)){
         echo "{$d->codigo} - mesa ({$d->mesa}) valor: {$d->total}<br>";
     }
