45bd3fef7eb957c0990247494769b9ba39e69109
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-10 17:27:57 -0400

    Atualizações

--- painel/vendas/print-2-new.php
+++ painel/vendas/print-2-new.php
@@ -2,9 +2,19 @@
     include("../../lib/includes.php");
     include "./conf.php";
 
+    function DataFormat($dt){
+        $dt = substr($dt, 0, -6);
+        list($d, $h) = explode("T",$dt);
+        list($a, $m, $d) = explode("-",$d);
+        return "{$d}/{$m}/{$a} {$h}";
+    }
+
     $query = "select a.*, b.mesa as mesa from vendas a left join mesas b on a.mesa = b.codigo where a.codigo = '{$_POST['cod']}'";
     $result = mysqli_query($con, $query);
     $p = mysqli_fetch_object($result);
+
+    $dados = json_decode($p->nf_json);
+
 
     // $retorno .= $query;
     //$tipo, $largura, $altura, $alinhamento, $registro)
@@ -17,6 +27,11 @@
     $retorno .= 'txt|1|2|left|Rua Bruxelas, 15, Manaus - AM'."\n";
     $retorno .= "txt|1|2|left|PEDIDO: ".str_pad($p->codigo, 5, "0", STR_PAD_LEFT)."  -  Mesa: {$p->mesa}"."\n";
     $retorno .= "txt|1|1|left|Pedido em : ".$data_pedido."\n\n";
+
+    if($p->nf_status == 'aprovado'){
+        $retorno .= 'txt|1|2|center|Documento Auxiliar da Nota Fiscal de Consumidor Eletronica'."\n";
+        $retorno .= 'txt|1|2|center|Não permite aproveitamento de crédito de ICMS'."\n\n";
+    }
 
     $retorno .= "txt|1|1|left|Produtos             Vl Uni              Vl Tot"."\n";
 
@@ -56,9 +71,27 @@
 
     $retorno .= "\ntxt|1|2|right|Pagar R$ ".number_format( ($valor_total + $p->taxa + $p->acrescimo - $d->desconto), 2, ',', '.').""."\n\n";
 
+    if($p->nf_status == 'aprovado'){
+
+    $retorno .= "\n\ntxt|1|1|center|Consulte pela Chave de Acesso em:"."\n";
+    $retorno .= "txt|1|1|center|".$dados->NFe->infNFeSupl->urlChave."\n";
+    $retorno .= "txt|1|1|center|".$dados->protNFe->infProt->chNFe."\n";
+
+    $retorno .= "\n\ntxt|1|1|center|CONSUMIDOR NÃO IDENTIFICADO"."\n";
+    $retorno .= "txt|1|1|center|NFCe n. ".str_pad($dados->NFe->infNFe->ide->nNF, 9, '0', STR_PAD_LEFT)." Série ".str_pad($dados->NFe->infNFe->ide->serie, 3, '0', STR_PAD_LEFT)." ".DataFormat($dados->NFe->infNFe->ide->dhEmi)."\n";
+    $retorno .= "txt|1|1|center|Protocolo de Autorização: {$dados->protNFe->infProt->nProt}\n";
+    $retorno .= "txt|1|1|center|Data de Autorização: ".DataFormat($dados->protNFe->infProt->dhRecbto)."\n\n";
+
+    $retorno .= "qrcode|8|8|center|{$dados->NFe->infNFeSupl->qrCode}";
+    $retorno .= "\n\ntxt|1|1|center|Tributos Incidentes (Lei Federal 12.741/2012)"."\n\n";
+
+    }else{
+
     $retorno .= "qrcode|8|8|center|https://notas.yobom.com.br/?{$p->codigo}";
     $retorno .= "\n\ntxt|1|1|center|".md5($p->codigo).""."\n\n";
     $retorno .= "txt|1|1|center|Yobom.com.br - ".date("d/m/Y H:i:s").""."\n";
+
+    }
 
     //$retorno = GerarPrint($retorno);
 
