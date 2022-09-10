80599848e08108ac58215ba10bcecfc5e4c81144
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-09 23:08:26 -0400

    Atualizações

---
+++ nf/lerXml.php
@@ -0,0 +1,9 @@
+<?php
+    include('config.php');
+
+    $query = "select * from vendas where codigo = ?";
+    $stmt = $PDO->prepare($query);
+    $stmt->execute([10834]);
+    $nota = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    echo $nota->nf_json;
