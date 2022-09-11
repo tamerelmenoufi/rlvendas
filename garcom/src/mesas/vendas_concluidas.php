680164ac5bc171129f5654e68624647aded2f878
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-10 22:12:47 -0400

    Atualizações

--- garcom/src/mesas/vendas_concluidas.php
+++ garcom/src/mesas/vendas_concluidas.php
@@ -30,10 +30,10 @@
     <h5 class="card-title">Venda: <b><?=str_pad($d->codigo, 5, "0", STR_PAD_LEFT)?></b> - MESA: <b><?=$d->mesa?></b></h5>
     <h6 class="card-subtitle mb-2 text-muted">Data Fechamento: <?=formata_datahora($d->data_finalizacao)?></h6>
     <p class="card-text">
-        <?="valor da compra: ".number_format($d->valor, 2, ",",false)?><br>
-        <?="Taxa de Serviço: ".number_format($d->taxa, 2, ",",false)?><br>
-        <?="Desconto: ".number_format($d->desconto, 2, ",",false)?><br>
-        <?="Valor Pago: ".number_format(($d->valor + $d->taxa - $d->desconto), 2, ",",false)?>
+        <?="valor da compra: R$".number_format($d->valor, 2, ",",false)?><br>
+        <?="Taxa de Serviço: R$".number_format($d->taxa, 2, ",",false)?><br>
+        <?="Desconto: R$".number_format($d->desconto, 2, ",",false)?><br>
+        <?="Valor Pago: R$".number_format(($d->valor + $d->taxa - $d->desconto), 2, ",",false)?>
     </p>
   </div>
 </div>
