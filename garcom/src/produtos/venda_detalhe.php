80fa7ffb7376bded83d0af74e9ffa77ab6d23edc
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-11 12:33:14 -0400

    Atualizações

--- garcom/src/produtos/venda_detalhe.php
+++ garcom/src/produtos/venda_detalhe.php
@@ -190,11 +190,11 @@
 
 <div class="PedidoBottomFixo">
     <div class="row">
-        <div class="col-3">
-
-        </div>
-        <div class="col-2">
-
+        <div class="col-5">
+            <button nota_fiscal="<?=$v->codigo?>" class="btn btn-success btn-block">
+                <i class="fa-solid fa-receipt"></i>
+                Nota Fiscal
+            </button>
         </div>
         <div class="col-2">
             <button
