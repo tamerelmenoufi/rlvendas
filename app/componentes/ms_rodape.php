97108b87859eadd7ca92032f3bf64cca0fe107c9
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-12 11:11:20 -0400

    Atualizações

--- app/componentes/ms_rodape.php
+++ app/componentes/ms_rodape.php
@@ -8,9 +8,9 @@
 ?>
 
 <div class="row">
-    <div class="col acao" componente="ms_popup" local="src/cliente/home.php"><i class="fa-solid fa-circle-user"></i><p>Cliente <?=$_SESSION['AppCliente']?></p></div>
-    <div class="col acao" componente="ms_popup_100" local="src/produtos/pedido.php"><i class="fa-solid fa-bell-concierge"></i><p>Pedido <?=$m->mesa?></p></div>
-    <div class="col acao" componente="ms_popup_100" local="src/produtos/pagar.php"><i class="fa-solid fa-circle-dollar-to-slot"></i><p>Pagar</p></div>
+    <div class="col acao" componente="ms_popup" local="src/cliente/home.php"><i class="fa-solid fa-circle-user"></i><p>Cliente</p></div>
+    <div class="col acao" componente="ms_popup_100" local="src/produtos/pedido.php"><i class="fa-solid fa-bell-concierge"></i><p>Pedido <?=str_pad($m->mesa , 3 , '0' , STR_PAD_LEFT)?></p></div>
+    <!-- <div class="col acao" componente="ms_popup_100" local="src/produtos/pagar.php"><i class="fa-solid fa-circle-dollar-to-slot"></i><p>Pagar</p></div> -->
 </div>
 <script>
     ///////////////
