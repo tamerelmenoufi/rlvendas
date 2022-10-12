9fdb8f9767242f698c94d5b337a085d5aa8e78f2
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-12 10:03:47 -0400

    Atualizações

--- painel/vendas/pesquisas/index.php
+++ painel/vendas/pesquisas/index.php
@@ -6,7 +6,7 @@
         <div class="col-md-4">
             <label for="mesa">Mesa</label>
             <select id="" class="form-control">
-                <option value="">::Selecione a mesa::</option>
+                <option value="">Todas as Mesa</option>
                 <?php
                 $q = "select * from mesas where situacao = '1' and deletado != '1' order by mesa";
                 $r = mysqli_query($con, $q);
@@ -19,11 +19,17 @@
             </select>
         </div>
         <div class="col-md-6">
-            <label for="data">Data</label>
-            <input type="text" class="form-control" />
+            <label for="data">Data do Pedido</label>
+            <input type="date" class="form-control" />
         </div>
         <div class="col-md-2 d-flex align-items-end">
             <button class="btn btn-success">Buscar</button>
         </div>
     </div>
 </div>
+
+<script>
+    $(function(){
+
+    })
+</script>
