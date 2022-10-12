dc8bcf21de43fe62afe5f5ad25ce2803324ef82a
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-12 11:35:49 -0400

    Atualizações

--- app/src/produtos/produtos.php
+++ app/src/produtos/produtos.php
@@ -123,11 +123,9 @@ while ($m = mysqli_fetch_array($m_r)) {
 <div class="PedidoPendentes">
     <b>ATENÇÃO!</b><br>
     Você possui pedidos que ainda não foram autorizados para o proparo.<br>Acesse sua lista de pedido pelo ícone <b>SINO <i class="fa-solid fa-bell-concierge"></i></b> localizado no topo desta página e autorize o preparo.
-    <p>
-        <button class="btn btn-danger" style="font-size:12px">
+        <button class="btn btn-warning" style="font-size:12px; margin-top:20px;">
             <i class="fa fa-thumbs-o-up" aria-hidden="true"></i> ok Endendi
         </button>
-    </p>
 </div>
 
 <!-- Informativo de pedidos ativos -->
