ab98e3b35ae39ace429d3183389857875f93cacb
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-04 15:35:44 -0400

    Atualizações

--- garcom/src/produtos/pagar.php
+++ garcom/src/produtos/pagar.php
@@ -198,16 +198,21 @@ where codigo = '{$_SESSION['AppVenda']}'";
                                     </select>
                                 </div>
                                 <div class="row" style="display:none;">
+
+
                                 <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
+                                <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
+                                    Dropdown button
+                                </button>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                                </div>
+
+
+
                                 </div>
                             </h5>
                         </div>
