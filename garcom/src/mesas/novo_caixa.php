4724a02060512e6a6dc1cd9cae6416da4603639e
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-27 23:20:37 +0300

    Atualizações

--- garcom/src/mesas/novo_caixa.php
+++ garcom/src/mesas/novo_caixa.php
@@ -74,7 +74,7 @@
 
 <script>
     $(function(){
-        $('#fisico_declarado').maskMoney();
+        $('#fisico_declarado').maskMoney({ thousands: false, decimal:'.' });
 
         if(terminal){
             $('#fisico_declarado').keyboard({type:'numpad'});;
