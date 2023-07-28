447a1f8c4d86f13ffe7994e5dbe25864086bef18
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-07-28 14:09:18 -0500

    Ataulizacoes

--- lib/includes.php
+++ lib/includes.php
@@ -20,17 +20,19 @@ if($_SERVER['HTTP_HOST'] == 'app.yobom.com.br'){
 }
 
 if($_SERVER['HTTP_HOST'] == 'yobom.com.br'){
+    
     foreach($_GET as $i => $v){
         $d = $i;
     }
-    if($d){
+
+    if(strlen($d)){
         $query = "select * from mesas where md5(mesa) = '{$d}' and situacao = '1' and deletado != '1'";
         $mesa = mysqli_fetch_object(mysqli_query($con, $query));  
         $_SESSION['AppPedido'] = $mesa->codigo;
+        header("location:https://yobom.com.br/rlvendas/app/?n=1");
+        exit();
     }
-    // $d = implode("&",$_GET);
-    header("location:https://yobom.com.br/rlvendas/app/?n=1");
-    exit();
+
 }
 
 
