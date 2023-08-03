68622584faee6131eaa99c239464f39e28052e7a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-03 07:50:48 -0500

    Atualizações

--- app/src/produtos/pedido.php
+++ app/src/produtos/pedido.php
@@ -126,7 +126,7 @@
 
 </style>
 <div class="PedidoTopoTitulo">
-    <h4>Pedido <?=str_pad($mesa , 3 , '0' , STR_PAD_LEFT)?></h4>
+    <h4>Pedido <?=str_pad($mesa->mesa , 3 , '0' , STR_PAD_LEFT)?></h4>
 </div>
 <div class="col" style="margin-bottom:60px; margin-top:20px;">
     <div class="col-12">
