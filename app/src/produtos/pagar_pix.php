a562f91598c3c79dc8273a2716c752d7259f3c10
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-09 15:03:30 -0500

    Atualizações

--- app/src/produtos/pagar_pix.php
+++ app/src/produtos/pagar_pix.php
@@ -114,7 +114,7 @@
                                         }
                                     }');
 
-                                    $dados = json_decode($retorno);
+                                    echo $dados = json_decode($retorno);
 
                                     $operadora_id = $dados->id;
                                     $forma_pagamento = $dados->payment_method_id;
