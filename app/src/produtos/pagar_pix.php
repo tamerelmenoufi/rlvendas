8ea8a5db4301112f3c682fe5ebaae254302b7c59
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-09 14:47:56 -0500

    Atualizações

--- app/src/produtos/pagar_pix.php
+++ app/src/produtos/pagar_pix.php
@@ -1,7 +1,7 @@
 <?php
     include("../../../lib/includes.php");
 
-    $query = "select
+    echo $query = "select
                     *
                 from vendas
                 where codigo = '{$_SESSION['codVenda']}'";
@@ -9,7 +9,7 @@
     $result = mysqli_query($con, $query);
     $d = mysqli_fetch_object($result);
 
-    $pos =  strripos($d->nome, " ");
+    // $pos =  strripos($d->nome, " ");
 
 ?>
 <style>
