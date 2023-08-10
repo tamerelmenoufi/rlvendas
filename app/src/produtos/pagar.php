27676748e0153126dcc2738f4230676a21754aa1
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-10 15:34:42 -0500

    Atualizações

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -136,7 +136,7 @@ where codigo = '{$_SESSION['AppVenda']}'";
                         <div class="col">
                         <?php
                             $valor_pago = 0;
-                            $query = "select * from status_venda where venda = '{$d->codigo}' where retorno->>'$.status' = 'approved';";
+                            $query = "select * from status_venda where venda = '{$d->codigo}' where retorno->>'$.status' = 'approved'";
                             $result = mysqli_query($con, $query);
                             while($p = mysqli_fetch_object($result)){
                             $op = json_decode($p->retorno);
