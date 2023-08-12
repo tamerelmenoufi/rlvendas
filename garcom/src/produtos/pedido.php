a312fb71281337ae5ef193fdaf50174273fa6036
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-12 16:53:24 -0500

    Atualizações

--- garcom/src/produtos/pedido.php
+++ garcom/src/produtos/pedido.php
@@ -228,8 +228,8 @@
         ?>
         <div class="card bg-light mb-3" style="padding-bottom:40px;">
             <div class="card-body">
-                <p Excluirproduto codigo="<?=$d->codigo?>" produto="<?=$pedido->categoria->descricao?> - <?=$pedido->medida->descricao?> <?=$sabores?>" style="position:absolute; right:-10px; top:-10px; width:auto;">
-                    <i class="fa-solid fa-circle-xmark" style="color:orange; font-size:30px; <?=$blqc?>"></i> <?=$d->pago?>
+                <p Excluirproduto codigo="<?=$d->codigo?>" produto="<?=$pedido->categoria->descricao?> - <?=$pedido->medida->descricao?> <?=$sabores?>" style="position:absolute; right:-10px; top:-10px; width:auto; <?=$blqc?>">
+                    <i class="fa-solid fa-circle-xmark" style="color:orange; font-size:30px;"></i>
                 <p>
                 <p Tempo>
                     <?=CalcTempo($d->data)?>
