e311d339875d2d4b646dc33aed211f4c7c953b0f
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-15 08:16:12 -0500

    Atualizações

--- app/src/produtos/produtos.php
+++ app/src/produtos/produtos.php
@@ -116,6 +116,16 @@ while ($m = mysqli_fetch_array($m_r)) {
     while ($p = mysqli_fetch_object($result)) {
         $detalhes = json_decode($p->detalhes, true);
         $detalhes_2 = [];
+        
+
+        foreach ($detalhes as $key => $val) :
+            if($val['valor'] > 0 and $val['quantidade'] > 0){
+                $val['ordem'] = $M[$key]['ordem'];
+                $detalhes_2[$key] = $val;
+            }
+        endforeach;
+        // print_r($detalhes_2);
+        if($detalhes_2){
         ?>
         <div class="card mb-3 item_button<?= $md5 ?>">
             <div class="row no-gutters">
@@ -130,12 +140,6 @@ while ($m = mysqli_fetch_array($m_r)) {
                         <small class="text-muted">
 
                             <?php
-                            foreach ($detalhes as $key => $val) :
-                                if($val['valor'] > 0){
-                                    $val['ordem'] = $M[$key]['ordem'];
-                                    $detalhes_2[$key] = $val;
-                                }
-                            endforeach;
 
                             aasort($detalhes_2, "ordem");
 
@@ -167,9 +171,8 @@ while ($m = mysqli_fetch_array($m_r)) {
                 </div>
             </div>
         </div>
-
-
         <?php
+        }
     }
     ?>
 </div>
