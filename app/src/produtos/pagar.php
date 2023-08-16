1121c1ae3de935600de3fe162ee8e7fa191555fc
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-16 10:11:12 -0500

    Atualizações

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -164,7 +164,7 @@ where codigo = '{$_SESSION['AppVenda']}'";
                                 type="button"
                                 class="adicionarPagamento btn btn-primary btn-lg btn-block"
                             >
-                                <i class="fa fa-credit"></i>
+                                <i class="fa fa-credit-card"></i>
                                 R$ <?=number_format(($d->total - $valor_pago),2,',','.')?><br>PIX
                             </button>
                         </div>
