6192da926b2fc6cd4bb8169a50bd954478dcadbc
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-16 07:28:51 -0500

    Atualizações

--- app/cartao/cartao.php
+++ app/cartao/cartao.php
@@ -18,7 +18,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cartão</title>
+    <meta name="theme-color" content="#990002"/>
+    <title>APP</title>
+    <?php include("../../lib/header.php"); ?>
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/app.css">
+
+
 </head>
 <body>
 
