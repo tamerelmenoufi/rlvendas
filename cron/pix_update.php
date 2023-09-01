ce17d4b2ca88ebd42cb5440b0dd44283cc7dc856
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-01 22:09:05 +0300

    Atualizações

---
+++ cron/pix_update.php
@@ -0,0 +1,62 @@
+<?php
+
+include("../lib/includes_off.php");
+
+
+$queryL = "select * from vendas where operadora_situacao = 'pending'";
+$resultL = mysqli_query($con, $queryL);
+while($v = mysqli_fetch_object($resultL)){
+
+    $PIX = new MercadoPago;
+    $retorno = $PIX->ObterPagamento($v->operadora_id);
+    $operadora_retorno = $retorno;
+    echo $retorno = json_decode($retorno);
+
+    echo "<hr>";
+    if($retorno->status == 'approved'){
+
+        $codigos = [];
+        $query = "SELECT * FROM vendas_produtos WHERE venda = '$v->codigo' and situacao = 'b'";
+        $result = mysqli_query($con, $query);
+        while($d = mysqli_fetch_object($result)){
+            $codigos[] = $d->codigo;
+        }
+        $codigos = implode(",", $codigos);
+
+        $ordem = strtotime("now");
+
+        $query = "UPDATE vendas_produtos SET situacao = 'p', ordem = '{$ordem}', pago = '1' WHERE codigo in ({$codigos})";
+        mysqli_query($con, $query);
+
+        mysqli_query($con, "update vendas set
+                            operadora_situacao = '{$retorno->status}',
+                            operadora_retorno = '{$operadora_retorno}',
+                            situacao = 'preparo'
+                        where codigo = '{$v->codigo}'
+                    ");
+
+        list($valorPago) = mysqli_fetch_row(mysqli_query($con, "select sum(valor) from vendas_pagamento where venda = '{$v->codigo}' and operadora_situacao = 'approved'"));
+
+        $caixa = mysqli_fetch_object(mysqli_query($con, "select * from caixa where situacao = '0'"));
+
+        mysqli_query($con, "INSERT INTO vendas_pagamento set
+                            caixa = '{$caixa->caixa}',
+                            venda = '{$v->codigo}',
+                            data = NOW(),
+                            forma_pagamento = 'pix',
+                            valor = '".($v->total - $valorPago)."',
+                            operadora = 'mercado_pago',
+                            operadora_situacao = 'approved',
+                            operadora_retorno = '{$operadora_retorno}'
+                    ");
+
+        mysqli_query($con, "UPDATE status_venda set
+                            retorno = '{$operadora_retorno}',
+                            data = NOW()
+                            where retorno->>'$.id' = '{$v->operadora_id}'
+                    ");
+    }
+
+}
+
+?>
---
+++ lib/includes_off.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(0);
+include "connect.php";
+$con = AppConnect();
+include "confYobom.php";
+include "config.php";
+include "utils.php";
+include "fn.php";
+include "vendor/mercado_pago/classes.php";
+// include "AppWapp.php";
+$md5 = md5(date("YmdHis"));
