aa38465650eebfc75c40d104c97312a44c7f58cf
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-10-07 20:26:37 +0300

    Atualizações

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -13,7 +13,8 @@
             }
             if($valor < $_POST['valor']){
                 mysqli_query($con, "update `vendas` set cupom = '{$c->codigo}', cupom_tipo = '{$c->tipo}', cupom_valor = 0 where codigo = '{$_SESSION['AppVenda']}'");
-            }           
+            }    
+            echo "Valor:".$valor;       
         }
 
 
