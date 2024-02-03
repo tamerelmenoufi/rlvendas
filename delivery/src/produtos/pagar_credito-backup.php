cf45bd85a121b6d81cbada5c0d3fb59895026b9a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-04 00:56:17 +0200

    atualizações

---
+++ delivery/src/produtos/pagar_credito-backup.php
@@ -0,0 +1,41 @@
+
+<style>
+      .PedidoTopoTitulo{
+        position:fixed;
+        left:0px;
+        top:0px;
+        width:100%;
+        height:60px;
+        background:#fff;
+        padding-left:70px;
+        padding-top:15px;
+        z-index:1;
+    }
+</style>
+
+  <div class="PedidoTopoTitulo">
+      <h4>Dados do Cartão</h4>
+  </div>
+
+<iframe src="cartao/cartao.php" frameborder="0" style="border:0; padding:0; margin:0; width:100%; height:650px;"></iframe>
+
+<script>
+    $(function(){
+        payConfirm = (cod) => {
+
+            $.ajax({
+                url:"cartao/status.php",
+                type:"POST",
+                data:{
+                    venda:cod
+                },
+                success:function(dados){
+                    $.alert(dados);
+                    PageClose();
+                    Carregando('none');
+                }
+            })
+
+        }
+    })
+</script>
--- delivery/src/produtos/pagar_credito.php
+++ delivery/src/produtos/pagar_credito.php
@@ -11,31 +11,118 @@
         padding-top:15px;
         z-index:1;
     }
+
+    .card small{
+        font-size:12px;
+        text-align:left;
+    }
+    .card input{
+        border:solid 1px #ccc;
+        border-radius:3px;
+        background-color:#eee;
+        color:#333;
+        font-size:20px;
+        text-align:center;
+        margin-bottom:5px;
+        width:100%;
+        text-transform:uppercase;
+    }
+
+    .alertas{
+        width:100%;
+        text-align:center;
+        background-color:#ffffff;
+        border:solid 1px #fd3e00;
+        color:#ff7d52;
+        text-align:center !important;
+        border-radius:7px;
+        font-size:11px !important;
+        font-weight:normal !important;
+        padding:5px;
+        margin-top:10px;
+        margin-bottom:10px;
+        display:<?=(($d->tentativas_pagamento < 3)?'block':'none')?>;
+    }
+
 </style>
 
   <div class="PedidoTopoTitulo">
       <h4>Dados do Cartão</h4>
   </div>
 
-<iframe src="cartao/cartao.php" frameborder="0" style="border:0; padding:0; margin:0; width:100%; height:650px;"></iframe>
 
+  <div class="card mb-3" style="background-color:#fafcff; padding:20px;">
+    <div class="row">
+            <div class="col-12">
+                <div class="card text-white bg-danger mb-3" style="padding:20px;">
+
+                    <small>Nome</small>
+                    <input type="text" id="cartao_nome" placeholder="NOME NO CARTÃO" value='' />
+                    <small>Número</small>
+                    <input inputmode="numeric" maxlength='19' type="text" id="cartao_numero" placeholder="0000 0000 0000 0000" value='' />
+                    <div class="row">
+                        <div class="col-4">
+                            <small>MM</small>
+                            <input inputmode="numeric" maxlength='2' type="text" id="cartao_validade_mes" placeholder="00" value='' />
+                        </div>
+                        <div class="col-4">
+                            <small>AAAA</small>
+                            <input inputmode="numeric" maxlength='4' type="text" id="cartao_validade_ano" placeholder="0000" value='' />
+                        </div>
+                        <div class="col-4">
+                            <small>CVV</small>
+                            <input inputmode="numeric" maxlength='4' type="text" id="cartao_ccv" placeholder="0000" value='' />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <small>BANDEIRAS</small>
+                            <div class="row">
+                                <div class="col">
+                                    <h2>
+                                        <i class="fa-brands fa-cc-mastercard"></i>
+                                        <i class="fa-brands fa-cc-visa"></i>
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="btn btn-secondary btn-block btn-lg" id="Pagar" hom="1" tentativas="<?=$d->tentativas_pagamento?>" loja="<?=$d->id_loja?>">
+                    <i class="fa fa-calculator" aria-hidden="true"></i>
+                    PAGAR R$ <?=number_format($_POST['valor_total'], 2, ',','.')?>
+                </button>
+
+                <div class="alertas animate__animated animate__fadeIn animate__infinite animate__slower">
+                    Atenção, você possui <span tentativa><?=$d->tentativas_pagamento?></span> tentativa(s)!
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
 <script>
     $(function(){
-        payConfirm = (cod) => {
 
-            $.ajax({
-                url:"cartao/status.php",
-                type:"POST",
-                data:{
-                    venda:cod
-                },
-                success:function(dados){
-                    $.alert(dados);
-                    PageClose();
-                    Carregando('none');
-                }
-            })
 
-        }
+        $("#cartao_numero").mask("9999 9999 9999 9999");
+        $("#cartao_validade_mes").mask("99");
+        $("#cartao_validade_ano").mask("9999");
+        $("#cartao_ccv").mask("9999");
+
+        $("#Pagar").click(function(){
+
+            reference = '<?="{$_POST['codVenda']}-".date("His")?>';
+            amount = '<?=number_format($_POST['valor_total'],2,".",false)?>';
+            cardholderName = $("#cartao_nome").val();
+            cardNumber = $("#cartao_numero").val();
+            expirationMonth = $("#cartao_validade_mes").val();
+            expirationYear = $("#cartao_validade_ano").val();
+            securityCode = $("#cartao_ccv").val();
+
+            $.alert(`Realização de pagamentos ${reference}`)
+        })
+
     })
 </script>
