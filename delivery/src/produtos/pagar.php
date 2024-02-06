95dc221508428c587f6b9293a488f1dcadc402b9
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-07 01:43:36 +0200

    Atualizações

--- delivery/src/produtos/pagar.php
+++ delivery/src/produtos/pagar.php
@@ -381,7 +381,7 @@ mysqli_query($con, "update vendas set taxa = '{$taxa_entrega}' where codigo = '{
                         </div>
                     </div>
                     <?php
-                    if((($d->total + $taxa_entrega - $d->cupom_valor) - $valor_pago) > 0 or !$blq){
+                    if((($d->total + $taxa_entrega - $d->cupom_valor) - $valor_pago) > 0 and !$blq){
                     ?>
                     <div class="row">
                         <div class="col">Escolha a forma de pagamento</div>
