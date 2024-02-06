006c3f5394f906e228b6880c90f3c507ac18770f
tamerelmenoufi <tamer@mohatron.com.br>
2024-02-06 10:08:57 -0400

    atualizações

--- delivery/src/produtos/pagar.php
+++ delivery/src/produtos/pagar.php
@@ -159,10 +159,6 @@ where codigo = '{$_SESSION['AppVenda']}'";
         font-size:14px;
         cursor:pointer;
     }
-    .valor_pendente{
-        font-size:30px !important;
-        color:green;
-    }
 
     .formas_pagamento{
         display:none;
@@ -180,7 +176,7 @@ where codigo = '{$_SESSION['AppVenda']}'";
                 <div class="card-body">
                     <div class="row">
 
-                        <div class="col-4">
+                        <div class="col-6">
                             <h5 class="card-title">
                                 <small>Valor da compra</small>
                                 <div style="font-size:18px !important; color:blue;">R$ <?=number_format($d->valor,2,',',false)?></div>
@@ -208,7 +204,7 @@ where codigo = '{$_SESSION['AppVenda']}'";
                             <h5 class="card-title">
                                 <small>Valor a pagar</small>
                                 <!-- <div class="valor" valor="<?=$d->valor?>">R$ <?=number_format($d->valor,2,',',false)?></div> -->
-                                <div class="valor_pendente" pendente="" valor="">R$ <?=number_format(($d->total-$d->cupom_valor),2,',',false)?></div>
+                                <div class="valor_pendente" style="font-size:18px !important; color:green;" pendente="" valor="">R$ <?=number_format(($d->total-$d->cupom_valor),2,',',false)?></div>
                             </h5>
                         </div>
                     </div>
