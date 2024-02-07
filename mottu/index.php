4fa425e85c645d5fbd08d8649fc3dae6503dcdce
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-07 12:44:58 +0200

    Atualizações

--- mottu/index.php
+++ mottu/index.php
@@ -1,12 +1,6 @@
 <?php
     include("../lib/includes.php");
 
-    $q = "select * from clientes where nome != '' limit 10";
-    $result = mysqli_query($con, $q);
-    while($d = mysqli_fetch_object($result)){
-        echo $d->nome."<br>";
-    }
-
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
     $_POST = json_decode(file_get_contents('php://input'), true);
 
