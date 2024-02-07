c14d94ef08004398350936a4635af2e2ccdbaca6
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-07 14:03:35 +0200

    Atualizações

---
+++ lib/vendor/mottu/pedido.php
@@ -0,0 +1,51 @@
+<?php
+    include("../../../lib/includes.php");
+
+    $query1 = "select 
+                        a.*,
+                        b.nome as Cnome,
+                        b.telefone as Ctelefone,
+                        b.logradouro as Clogradouro,
+                        b.numero as Cnumero,
+                        b.ponto_referencia as Cponto_referencia,
+                        b.bairro as Cbairro,
+                        b.cep as Ccep 
+                from vendas a left join clientes b on a.cliente = b.codigo where a.codigo = '61023'";
+    $result1 = mysqli_query($con, $query1);
+    $d1 = mysqli_fetch_object($result1);
+
+
+    $json = '{
+        "code": "'.$d->codigo.'",
+        "fullCode": "bk-'.$d->codigo.'",
+        "preparationTime": 0,
+        "previewDeliveryTime": false,
+        "sortByBestRoute": false,
+        "deliveries": [
+            {
+            "code": "'.$d->codigo.'",
+            "confirmation": {
+                "mottu": true
+            },
+            "name": "'.$d->Cnome.'",
+            "phone": "'.trim(str_replace(array(' ','-','(',')'), false, $d->Ctelefone)).'",
+            "observation": "'.$d->observacoes.'",
+            "address": {
+                "street": "'.$d->Clogradouro.'",
+                "number": "'.$d->Cnumero.'",
+                "complement": "'.$d->Cponto_referencia.'",
+                "neighborhood": "'.$d->Cbairro.'",
+                "city": "Manaus",
+                "state": "AM",
+                "zipCode": "'.trim(str_replace(array(' ','-'), false, $d->Ccep)).'"
+            },
+            "onlinePayment": true,
+            "productValue": '.($d->valor+$d->taxa-$d->desconto+$d->acrescimo).'
+            }
+        ]
+        }';
+
+    $mottu = new mottu;
+    $retorno1 = $mottu->NovoPedido($json);
+    $retorno = json_decode($retorno1);
+    print_r($retorno);
