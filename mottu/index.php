bea99e39e499fba4a706e3fb80bf388a05334ae4
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-07 12:43:58 +0200

    Atualizações

--- mottu/index.php
+++ mottu/index.php
@@ -1,1 +1,17 @@
-INTEGRAÇÃO MOTTU
+<?php
+    include("../lib/includes.php");
+
+    $q = "select * from clientes limit 10";
+    $result = mysqli_query($con, $q);
+    while($d = mysqli_fetch_object($result)){
+        echo $d->nome."<br>";
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+    if($_GET and !$_POST) $_POST = $_GET;
+
+    $dadosLog = print_r($_POST,true);
+
+    file_put_contents("log-".date("YmdHis").".txt", $dadosLog);
