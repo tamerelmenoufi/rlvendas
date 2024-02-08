a9782f1ce15047a8e097d6de5f92959d7722cbf8
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-08 14:01:00 +0200

    Atualizações

--- mottu/index.php
+++ mottu/index.php
@@ -2,11 +2,23 @@
     include("../lib/includes.php");
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
-    $_POST = json_decode(file_get_contents('php://input'), true);
+    $json = file_get_contents('php://input');
+    $_POST = json_decode($json, true);
 
     if($_GET and !$_POST) $_POST = $_GET;
 
-    // if($_POST){
+    if($_POST){
+
+        $query = "insert into delivery_logs set 
+                                                venda = '{$_POST['CodigoExterno']}',
+                                                data = NOW(),
+                                                delivery = '{$json}'
+                ";
+        mysqli_query($con, $query);
+
+        $query = "UPDATE vendas set delivery = {$json} where codigo = '{$_POST['CodigoExterno']}'";
+        mysqli_query($con, $query);
+
         $dadosLog = print_r($_POST,true);
         file_put_contents("log-".date("YmdHis").".txt", $dadosLog);        
-    // }
+    }
