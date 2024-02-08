4b8c8b10952f368cb8fb2e76e307917fafcb6b80
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-09 00:05:46 +0200

    Atualizações

--- delivery/src/cliente/pedidos.php
+++ delivery/src/cliente/pedidos.php
@@ -53,7 +53,7 @@
                     </div>
 
                     <div class="d-flex justify-content-between">
-                        <div>Valor</div>
+                        <div>Total</div>
                         <span>R$ <?=number_format(($d->valor + $d->taxa - $d->desconto + $d->acrescimo), 2,',', false)?></span>
                     </div>
                     <?php
