bab3ce3ac467da11499425363d877e9d1afcc461
tamerelmenoufi <tamer@mohatron.com.br>
2024-02-09 09:47:31 -0400

    atualizações

--- loja/src/index.php
+++ loja/src/index.php
@@ -5,8 +5,56 @@
     $result = mysqli_query($con, $query);
     while($d = mysqli_fetch_object($result)){
 ?>
-<div>
-    <?=$d->codigo?>
+<div class="card">
+    <h5 class="card-header">Pedido #<?=$d->codigo?></h5>
+    <div class="card-body">
+        <div class="d-flex justify-content-between">
+            <div>Valor</div>
+            <span>R$ <?=number_format($d->valor, 2,',', false)?></span>
+        </div>
+
+        <div class="d-flex justify-content-between">
+            <div>Taxa Entrega</div>
+            <span>R$ <?=number_format($d->taxa, 2,',', false)?></span>
+        </div>
+
+        <div class="d-flex justify-content-between">
+            <div>Desconto</div>
+            <span>R$ <?=number_format($d->desconto, 2,',', false)?></span>
+        </div>
+
+        <div class="d-flex justify-content-between">
+            <div>Acrescimo</div>
+            <span>R$ <?=number_format($d->acrescimo, 2,',', false)?></span>
+        </div>
+
+        <div class="d-flex justify-content-between">
+            <div><b>Total</b></div>
+            <span><b>R$ <?=number_format(($d->valor + $d->taxa - $d->desconto + $d->acrescimo), 2,',', false)?></b></span>
+        </div>
+        <?php
+        if($delivery->deliveryMan->id){
+        ?>
+        <div class="d-flex justify-content-between mt-3">
+            <div>Entregador</div>
+            <span><?=$delivery->deliveryMan->name?></span>
+        </div>
+        <div class="d-flex justify-content-between">
+            <div>Telefone (Entregador)</div>
+            <span><?='('.$delivery->deliveryMan->ddd.') '.$delivery->deliveryMan->phone?></span>
+        </div>
+        <div class="d-flex justify-content-between">
+            <div>Código Recebimento</div>
+            <span><b><?=$delivery->returnCode?></b></span>
+        </div>
+        <?php
+        }
+        ?>
+        <div class="d-flex justify-content-start">
+            <div style="padding-right:7px;">Situação</div>
+            <span><?=(($d->situacao_entrega)?:'Em Produção')?></span>
+        </div>
+    </div>
 </div>
 <?php
     }
