02802ae17470cd66f30755b8f542839c65a77f62
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-21 22:06:05 +0200

    Atualizações

--- panel/src/estoque/fornecedores.php
+++ panel/src/estoque/fornecedores.php
@@ -5,6 +5,17 @@
         $_SESSION['cod_lancamento'] = $_POST['lancamento'];
     }
 
+    if($_POST['filtro'] == 'filtrar'){
+        $_SESSION['textoBusca'] = $_POST['campo'];
+      }elseif($_POST['filtro']){
+        $_SESSION['textoBusca'] = false;
+      }
+  
+      if($_SESSION['textoBusca']){
+        $cpf = str_replace( ['.','-','/'], false, $_SESSION['textoBusca']);
+        $where = " and nome_razao_social like '%{$_SESSION['textoBusca']}%' or REPLACE( REPLACE( REPLACE( cpf_cnpj, '/', '' ), '.', '' ), '-', '' ) = '{$cpf}' ";
+      }
+
     $query = "select * from lancamentos where codigo = '{$_SESSION['cod_lancamento']}'";
     $result = sisLog($query);
     $d = mysqli_fetch_object($result);
@@ -21,7 +32,55 @@
 <h6>Selecione um Fornecedor</h6>
 <div class="input-group">
 <label class="input-group-text" for="inputGroupFile01">Buscar por </label>
-    <input campoBusca type="text" class="form-control" value="<?=$_SESSION['usuarioBusca']?>" aria-label="Digite a informação para a busca">
+    <input textoBusca type="text" class="form-control" value="<?=$_SESSION['textoBusca']?>" aria-label="Digite a informação para a busca">
     <button filtro="filtrar" class="btn btn-outline-secondary" type="button">Buscar</button>
     <button filtro="limpar" class="btn btn-outline-danger" type="button">limpar</button>
 </div>
+<table>
+    <thead>
+        <tr>
+            <th>Nome/Razão Social</th>
+            <th>CPF</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php
+    $query = "select * from fornecedores {$where} order by nome_razao_social limit 100";
+    $result = sisLog($query);
+    while($d = mysqli_fetch_object($result)){
+?> 
+        <tr>
+            <td><?=$d->nome_razao_social?></td>
+            <td><?=$d->cpf_cnpj?></td>
+            <td>
+                <i class="fa-regular fa-square-plus" style="cursor:pointer"></i>
+            </td>
+        </tr>
+<?php
+    }
+?>
+    </tbody>
+</table>
+
+
+<script>
+    $(function(){
+
+        $("button[filtro]").click(function(){
+          filtro = $(this).attr("filtro");
+          campo = $("input[textoBusca]").val();
+          $.ajax({
+              url:"src/estoque/fornecedores.php",
+              type:"POST",
+              data:{
+                  filtro,
+                  campo
+              },
+              success:function(dados){
+                  $("#paginaHome").html(dados);
+              }
+          })
+        })
+
+    })
+</script>
