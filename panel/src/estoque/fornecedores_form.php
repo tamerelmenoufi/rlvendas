6e1dd13394a8a291a926d0848854b34c8bf55018
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-21 23:17:28 +0200

    Atualizações

--- panel/src/estoque/fornecedores_form.php
+++ panel/src/estoque/fornecedores_form.php
@@ -28,7 +28,7 @@
 
         $retorno = [
             'status' => true,
-            'codigo' => $cod
+            'codigo' => $query
         ];
 
         echo json_encode($retorno);
@@ -148,7 +148,7 @@
                     mimeType: 'multipart/form-data',
                     data: campos,
                     success:function(dados){
-                    //console.log(dados)
+                    console.log(dados)
                         // if(dados.status){
                             $.ajax({
                                 url:"src/estoque/fornecedores.php",
