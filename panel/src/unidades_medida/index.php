2b2a9ccd6f287373f816b56d36e188953b88c4d2
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-22 15:07:19 +0200

    Atualizações

---
+++ panel/src/unidades_medida/index.php
@@ -0,0 +1,161 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/rlvendas/panel/lib/includes.php");
+
+    if($_POST['delete']){
+      $query = "delete from unidades_medida where codigo = '{$_POST['delete']}'";
+      // $query = "update unidades_medida set deletado = '1' where codigo = '{$_POST['delete']}'";
+      sisLog($query);
+    }
+
+?>
+<style>
+  .btn-perfil{
+    padding:5px;
+    border-radius:8px;
+    color:#fff;
+    background-color:#a1a1a1;
+    cursor: pointer;
+  }
+  td, th{
+    white-space: nowrap;
+  }
+  .label{
+    font-size:10px;
+    color:#a1a1a1;
+  }
+</style>
+<div class="col">
+  <div class="m-3">
+
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <h5 class="card-header">Lista das Unidades de Medida</h5>
+          <div class="card-body">
+            <div class="d-flex justify-content-end mb-3">
+                <button
+                    novoCadastro
+                    class="btn btn-success btn-sm"
+                    data-bs-toggle="offcanvas"
+                    href="#offcanvasDireita"
+                    role="button"
+                    aria-controls="offcanvasDireita"
+                    style="margin-left:20px;"
+                >Novo</button>
+            </div>
+
+            
+
+            <div class="table-responsive">
+              <table class="table table-striped table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">Unidade</th>
+                    <th scope="col">Ações</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                    $query = "select * from unidades_medida where deletado != '1' {$where} order by nome asc";
+                    $result = sisLog($query);
+                    
+                    while($d = mysqli_fetch_object($result)){
+                  ?>
+                  <tr>
+                    <td class="w-100"><?=$d->unidade?></td>
+                    <td>
+                      <button
+                        class="btn btn-primary"
+                        edit="<?=$d->codigo?>"
+                        data-bs-toggle="offcanvas"
+                        href="#offcanvasDireita"
+                        role="button"
+                        aria-controls="offcanvasDireita"
+                      >
+                        Editar
+                      </button>
+                      <?php
+                      if($d->codigo != 1){
+                      ?>
+                      <button class="btn btn-danger" delete="<?=$d->codigo?>">
+                        Excluir
+                      </button>
+                      <?php
+                      }
+                      ?>
+                    </td>
+                  </tr>
+                  <?php
+                    }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+<script>
+    $(function(){
+        Carregando('none');
+
+        $("button[novoCadastro]").click(function(){
+            $.ajax({
+                url:"src/unidades_medida/form.php",
+                success:function(dados){
+                    $(".LateralDireita").html(dados);
+                }
+            })
+        })
+
+
+        $("button[edit]").click(function(){
+            cod = $(this).attr("edit");
+            $.ajax({
+                url:"src/unidades_medida/form.php",
+                type:"POST",
+                data:{
+                  cod
+                },
+                success:function(dados){
+                    $(".LateralDireita").html(dados);
+                }
+            })
+        })
+
+
+        $("button[delete]").click(function(){
+            deletar = $(this).attr("delete");
+            $.confirm({
+                content:"Deseja realmente excluir o cadastro ?",
+                title:false,
+                buttons:{
+                    'SIM':function(){
+                        $.ajax({
+                            url:"src/unidades_medida/index.php",
+                            type:"POST",
+                            data:{
+                                delete:deletar
+                            },
+                            success:function(dados){
+                                $("#paginaHome").html(dados);
+                            }
+                        })
+                    },
+                    'NÃO':function(){
+
+                    }
+                }
+            });
+
+        })
+
+
+    })
+</script>
