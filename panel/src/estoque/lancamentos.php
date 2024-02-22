e4b713f2dbecc650009c6ae31c9694e510b304c8
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-22 21:16:12 +0200

    Atualizações

--- panel/src/estoque/lancamentos.php
+++ panel/src/estoque/lancamentos.php
@@ -130,8 +130,7 @@
                 </thead>
                 <tbody>
                   <?php
-                    echo $query = "select a.*, b.nome_razao_social from lancamentos a left join fornecedores b on a.fornecedor = b.codigo where a.deletado != '1' {$where} order by a.data desc";
-                    $query = "select a.*, b.nome_razao_social from lancamentos a left join fornecedores b on a.fornecedor = b.codigo where a.deletado != '1' order by a.data desc";
+                    $query = "select a.*, b.nome_razao_social from lancamentos a left join fornecedores b on a.fornecedor = b.codigo where a.deletado != '1' {$where} order by a.data desc";
                     $result = sisLog($query);
                     
                     while($d = mysqli_fetch_object($result)){
