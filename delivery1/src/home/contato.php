32158207707de2d51ad6c6aca275b80eba270123
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-03 09:39:32 +0200

    Atualizações

--- delivery1/src/home/contato.php
+++ delivery1/src/home/contato.php
@@ -23,10 +23,10 @@
   </div>
   <div class="card-body">
     <p class="card-text">Utilize um dos meios abaixo para falar conosco.</p>
+  </div>
     <ul class="list-group list-group-flush">
         <li class="list-group-item"><i class="fa-solid fa-phone-volume"></i> <a href="tel:(92) 99321-6300">(92) 99321-6300</a></li>
         <li class="list-group-item"><i class="fa-brands fa-whatsapp"></i> <a href="https://api.whatsapp.com/send?phone=5592993216300">Contato chat WhatsApp</a></li>
         <li class="list-group-item"><i class="fa-solid fa-envelope"></i> <a href="mailto:atendimento@yobom.com.br">atendimento@yobom.com.br</a></li>
-  </ul>
-  </div>
+    </ul>
 </div>
