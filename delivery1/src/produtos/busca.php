7af3458759cfa1c1df6a547c444651d6f0d38ebd
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-03 10:04:33 +0200

    Atualizações

--- delivery1/src/produtos/busca.php
+++ delivery1/src/produtos/busca.php
@@ -159,36 +159,34 @@ function aasort(&$array, $key)
                         </div>
                     </div>
                 </div>
-                <div class="row no-gutters">
-                    <div class="d-flex flex-row-reverse bd-highlight">
+                <div class="d-flex flex-row-reverse bd-highlight">
 
-                    <?php
+                <?php
 
-                    aasort($detalhes_2, "ordem");
+                aasort($detalhes_2, "ordem");
 
-                    foreach ($detalhes_2 as $key2 => $val) {
-                        if ($val["quantidade"] > 0) {
-                            ?>
-                            <button
-                                    acao_medida
-                                    opc="<?= $val["quantidade"]; ?>"
-                                    produto="<?= $p->codigo ?>"
-                                    titulo='<?= "{$d->categoria} - {$p->produto} ({$M[$key2]["descricao"]})" ?>'
-                                    categoria='<?= $d->codigo ?>'
-                                    medida='<?= $val["quantidade"]; ?>'
-                                    valor='<?= $val['valor']; ?>'
-                                    class="btn btn-lg"
-                                    style="height:auto; background-color:#a80e13; border:0; font-size:12px; color:#fff; font-weight:bold; line-height: 1.2;"
-                            >
-                                <?= $M[$key2]['descricao']; ?><br>
-                                <?= number_format($val['valor'], 2, '.', false) ?>
-                            </button>
-                            <?php
-                        }
+                foreach ($detalhes_2 as $key2 => $val) {
+                    if ($val["quantidade"] > 0) {
+                        ?>
+                        <button
+                                acao_medida
+                                opc="<?= $val["quantidade"]; ?>"
+                                produto="<?= $p->codigo ?>"
+                                titulo='<?= "{$d->categoria} - {$p->produto} ({$M[$key2]["descricao"]})" ?>'
+                                categoria='<?= $d->codigo ?>'
+                                medida='<?= $val["quantidade"]; ?>'
+                                valor='<?= $val['valor']; ?>'
+                                class="btn btn-lg"
+                                style="height:auto; background-color:#a80e13; border:0; font-size:12px; color:#fff; font-weight:bold; line-height: 1.2;"
+                        >
+                            <?= $M[$key2]['descricao']; ?><br>
+                            <?= number_format($val['valor'], 2, '.', false) ?>
+                        </button>
+                        <?php
                     }
-                    ?>
+                }
+                ?>
 
-                    </div>
                 </div>
 
             </div>
