4be65b53190c2a9c2142308218fc534e122234bd
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-10 02:21:55 +0200

    Atualizações

--- panel/src/componentes/menu_lateral/menu.php
+++ panel/src/componentes/menu_lateral/menu.php
@@ -52,6 +52,14 @@
       </div>
     </div>
 
+    <div class="row mb-1">
+      <div class="col">
+        <a url="src/pagamentos/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-regular fa-user col-1"></i> <span class="col-11">Pagamentos</span>
+        </a>
+      </div>
+    </div>
+
 
     <hr>
 
---
+++ panel/src/pagamentos/form.php
@@ -0,0 +1,134 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/rlvendas/panel/lib/includes.php");
+
+
+    if($_POST['acao'] == 'salvar'){
+
+        $data = $_POST;
+        $attr = [];
+
+        unset($data['codigo']);
+        unset($data['acao']);
+
+        foreach ($data as $name => $value) {
+            $attr[] = "{$name} = '" . mysqli_real_escape_string($con, $value) . "'";
+        }
+
+        $attr = implode(', ', $attr);
+
+        if($_POST['codigo']){
+            $query = "update pagamentos set {$attr} where codigo = '{$_POST['codigo']}'";
+            sisLog($query);
+            $cod = $_POST['codigo'];
+        }else{
+            $query = "insert into pagamentos set {$attr}";
+            sisLog($query);
+            $cod = mysqli_insert_id($con);
+        }
+
+        $retorno = [
+            'status' => true,
+            'codigo' => $cod
+        ];
+
+        echo json_encode($retorno);
+
+        exit();
+    }
+
+
+    $query = "select * from pagamentos where codigo = '{$_POST['cod']}'";
+    $result = sisLog($query);
+    $d = mysqli_fetch_object($result);
+?>
+<style>
+    .Titulo<?=$md5?>{
+        position:absolute;
+        left:60px;
+        top:8px;
+        z-index:0;
+    }
+</style>
+<h4 class="Titulo<?=$md5?>">Cadastro das Mesas</h4>
+    <form id="form-<?= $md5 ?>">
+        <div class="row">
+            <div class="col">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="pagamento" name="pagamento" placeholder="Identificação da Mesa" value="<?=$d->pagamento?>">
+                    <label for="pagamento">Pagamento*</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição da mesa" value="<?=$d->descricao?>">
+                    <label for="descricao">Descrição*</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <select name="situacao" class="form-control" id="situacao">
+                        <option value="1" <?=(($d->situacao == '1')?'selected':false)?>>Liberado</option>
+                        <option value="0" <?=(($d->situacao == '0')?'selected':false)?>>Bloqueado</option>
+                    </select>
+                    <label for="email">Situação</label>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div style="display:flex; justify-content:end">
+                    <button type="submit" class="btn btn-success btn-ms">Salvar</button>
+                    <input type="hidden" id="codigo" value="<?=$_POST['cod']?>" />
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <script>
+        $(function(){
+            Carregando('none');
+
+            $('#form-<?=$md5?>').submit(function (e) {
+
+                e.preventDefault();
+
+                var codigo = $('#codigo').val();
+                var campos = $(this).serializeArray();
+
+                if (codigo) {
+                    campos.push({name: 'codigo', value: codigo})
+                }
+
+                campos.push({name: 'acao', value: 'salvar'})
+
+                Carregando();
+
+                $.ajax({
+                    url:"src/pagamentos/form.php",
+                    type:"POST",
+                    typeData:"JSON",
+                    mimeType: 'multipart/form-data',
+                    data: campos,
+                    success:function(dados){
+                    //console.log(dados)
+                        // if(dados.status){
+                            $.ajax({
+                                url:"src/pagamentos/index.php",
+                                type:"POST",
+                                success:function(dados){
+                                    $("#paginaHome").html(dados);
+                                    let myOffCanvas = document.getElementById('offcanvasDireita');
+                                    let openedCanvas = bootstrap.Offcanvas.getInstance(myOffCanvas);
+                                    openedCanvas.hide();
+                                }
+                            });
+                        // }
+                    },
+                    error:function(erro){
+
+                        // $.alert('Ocorreu um erro!' + erro.toString());
+                        //dados de teste
+                    }
+                });
+
+            });
+
+        })
+    </script>
