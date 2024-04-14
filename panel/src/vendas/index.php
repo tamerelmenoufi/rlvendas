28c7f5697b683214361d386e18b865d110679eda
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-14 17:48:15 +0200

    Atualizações

--- panel/src/vendas/index.php
+++ panel/src/vendas/index.php
@@ -5,8 +5,28 @@
         'aberto' => " and deletado != '1' and caixa = '0' and app = 'mesa' ",
         'paga' => " and deletado != '1' and caixa != '0' and app = 'mesa' and situacao = 'pago'",
     ];
+?>
 
+<div class="row">
+    <div class="col">
+        <div class="input-group mb-3">
+            <span class="input-group-text">Tipo</span>
+            <select id="busca_tipo">
+                <option value="">Todos</option>
+                <option value="garcom">Atendimento pelo Garçom</option>
+                <option value="mesa">Pedido feito pelo Cliente (na mesa)</option>
+                <option value="delivery">Pedido pelo Delivery</option>
+            </select>
+            <span class="input-group-text">Em</span>
+            <input type="date" class="form-control" >
+            <span class="input-group-text">até</span>
+            <input type="date" class="form-control" >
+            <button class="btn btn-outline-secondary" type="button" id="button-addon1">Acahar</button>
+        </div>
+    </div>
+</div>
 
+<?php
     echo $query = "select * from vendas where data_pedido like '".date("Y-m-d")."%' {$tipo[$_GET['tipo']]}";
     $result = mysqli_query($con, $query);
     while($d = mysqli_fetch_object($result)){
