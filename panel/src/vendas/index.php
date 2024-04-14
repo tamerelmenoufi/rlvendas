d72065a09d0abe182e470f2ed08e75403a5dd423
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-14 21:12:45 +0200

    Atualizações

--- panel/src/vendas/index.php
+++ panel/src/vendas/index.php
@@ -50,7 +50,7 @@
 </div>
 
 <?php
-    echo $query = "select a.* from vendas a where a.deletado != '1' {$where} {$tipo[$_GET['filtro']]} order by a.codigo desc limit 30";
+    echo $query = "select a.* from vendas a where a.deletado != '1' {$where} {$tipo[$_GET['filtro']]} order by a.codigo desc".((!$_SESSION['data_inicial'])?" limit 50 ":false);
     $result = mysqli_query($con, $query);
     while($d = mysqli_fetch_object($result)){
 
