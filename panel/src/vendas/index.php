e52980c52c38d1580d655aaf301f4ea4f583130d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-14 23:07:03 +0200

    Atualizações

--- panel/src/vendas/index.php
+++ panel/src/vendas/index.php
@@ -52,7 +52,7 @@
         <div class="m-3">
             <div class="row">
 <?php
-    echo $query = "select a.* from vendas a where a.deletado != '1' {$where} {$tipo[$_SESSION['busca_tipo']]} order by a.codigo desc".((!$_SESSION['data_inicial'])?" limit 50 ":false);
+    $query = "select a.* from vendas a where a.deletado != '1' {$where} {$tipo[$_SESSION['busca_tipo']]} order by a.codigo desc".((!$_SESSION['data_inicial'])?" limit 50 ":false);
     $result = mysqli_query($con, $query);
     while($d = mysqli_fetch_object($result)){
 
