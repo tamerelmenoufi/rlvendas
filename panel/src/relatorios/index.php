69585cfe778b1de8504dabec315daf8f7acc027e
tamerelmenoufi <tamer@mohatron.com.br>
2024-04-29 10:08:31 -0400

    Atualizações

--- panel/src/relatorios/index.php
+++ panel/src/relatorios/index.php
@@ -2,7 +2,31 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/rlvendas/panel/lib/includes.php");
 
 ?>
-<h1>Relatórios</h1>
+
+
+<div class="row g-0">
+    <div class="col">
+        <div class="m-3">
+            <div class="input-group">
+                <span class="input-group-text">Tipo</span>
+                <select id="busca_tipo" class="form-select">
+                    <option value="garcom" <?=(($_SESSION['busca_tipo'] == 'garcom')?'selected':false)?>>Atendimento pelo Garçom</option>
+                    <option value="cliente" <?=(($_SESSION['busca_tipo'] == 'cliente')?'selected':false)?>>Pedido pelo Cliente (na mesa)</option>
+                    <option value="viagem" <?=(($_SESSION['busca_tipo'] == 'viagem')?'selected':false)?>>Pedido para viagem</option>
+                    <option value="delivery" <?=(($_SESSION['busca_tipo'] == 'delivery')?'selected':false)?>>Pedido pelo Delivery</option>
+                </select>
+                <span class="input-group-text">Em</span>
+                <input id="data_inicial" value="<?=$_SESSION['data_inicial']?>" type="date" class="form-control" >
+                <span class="input-group-text">até</span>
+                <input id="data_final" value="<?=$_SESSION['data_final']?>" type="date" class="form-control" >
+                <button buscar class="btn btn-outline-secondary" type="button" id="button-addon1">Listar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 <script>
     $(function(){
         Carregando('none')
