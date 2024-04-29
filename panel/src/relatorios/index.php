3c78479f231ba993d07d6fd2a72ae97cd01431c6
tamerelmenoufi <tamer@mohatron.com.br>
2024-04-29 10:26:00 -0400

    Atualizações

--- panel/src/relatorios/index.php
+++ panel/src/relatorios/index.php
@@ -1,24 +1,28 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/rlvendas/panel/lib/includes.php");
 
+
+    if($_POST['acao'] == 'busca'){
+
+        $_SESSION['data_inicial'] = $_POST['data_inicial'];
+        $_SESSION['data_final'] = $_POST['data_final'];
+
+
+
+    }
+
 ?>
 
 
 <div class="row g-0">
     <div class="col">
         <div class="m-3">
+            <h4>Relatório de Vendas</h4>
             <div class="input-group">
-                <span class="input-group-text">Tipo</span>
-                <select id="busca_tipo" class="form-select">
-                    <option value="garcom" <?=(($_SESSION['busca_tipo'] == 'garcom')?'selected':false)?>>Atendimento pelo Garçom</option>
-                    <option value="cliente" <?=(($_SESSION['busca_tipo'] == 'cliente')?'selected':false)?>>Pedido pelo Cliente (na mesa)</option>
-                    <option value="viagem" <?=(($_SESSION['busca_tipo'] == 'viagem')?'selected':false)?>>Pedido para viagem</option>
-                    <option value="delivery" <?=(($_SESSION['busca_tipo'] == 'delivery')?'selected':false)?>>Pedido pelo Delivery</option>
-                </select>
                 <span class="input-group-text">Em</span>
-                <input id="data_inicial" value="<?=$_SESSION['data_inicial']?>" type="date" class="form-control" >
+                <input id="data_inicial" value="<?=$_SESSION['vendas_data_inicial']?>" type="date" class="form-control" >
                 <span class="input-group-text">até</span>
-                <input id="data_final" value="<?=$_SESSION['data_final']?>" type="date" class="form-control" >
+                <input id="data_final" value="<?=$_SESSION['vendas_data_final']?>" type="date" class="form-control" >
                 <button buscar class="btn btn-outline-secondary" type="button" id="button-addon1">Listar</button>
             </div>
         </div>
@@ -30,5 +34,36 @@
 <script>
     $(function(){
         Carregando('none')
+
+
+        $("button[buscar]").click(function(){
+            data_inicial = $("#data_inicial").val()
+            data_final = $("#data_final").val()
+            if(data_inicial && data_final){
+
+                $.ajax({
+                    url:"src/relatorios/index.php",
+                    data:{
+                        data_inicial,
+                        data_final,
+                        acao:'busca'
+                    },
+                    type:"POST",
+                    success:function(dados){
+                        $("#paginaHome").html(dados);
+                    }
+                });
+
+            }else{
+
+                $.alert({
+                    title:"Erro Busca",
+                    content:"Informe o intervalo de datas para a busca",
+                    type:"red"
+                })
+                return false;
+
+            }
+        })
     })
 </script>
