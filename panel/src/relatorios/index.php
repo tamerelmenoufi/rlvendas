5cd05b949ff82db599ae2ec90ecf190761e86b1f
tamerelmenoufi <tamer@mohatron.com.br>
2024-04-29 10:04:11 -0400

    Atualizações

--- panel/src/relatorios/index.php
+++ panel/src/relatorios/index.php
@@ -1,4 +1,10 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/rlvendas/panel/lib/includes.php");
-    
+
 ?>
+<h1>Relatórios</h1>
+<script>
+    $(function(){
+        Carregando('none')
+    })
+</script>
