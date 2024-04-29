f3d7f979159ae88b85ab2b43fdfd7c49ac34ac9e
tamerelmenoufi <tamer@mohatron.com.br>
2024-04-29 09:59:26 -0400

    Atualizações

--- panel/src/componentes/menu_lateral/menu.php
+++ panel/src/componentes/menu_lateral/menu.php
@@ -53,7 +53,16 @@
           <i class="fa-regular fa-user col-1"></i> <span class="col-11">Delivery</span>
         </a>
       </div>
-    </div>   
+    </div>
+    
+    <div class="row mb-1">
+      <div class="col">
+        <a url="src/relatorios/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-regular fa-user col-1"></i> <span class="col-11">Relatório</span>
+        </a>
+      </div>
+    </div>
+
 
     <hr>
 
