49c85ef4251da96142ced716e886da9ab3c0be63
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-16 00:28:59 +0300

    Atualizações

--- garcom/src/mesas/vendas_concluidas.php
+++ garcom/src/mesas/vendas_concluidas.php
@@ -100,32 +100,34 @@
             </div>
         </div>
 
-        <table class="table table-hover" style="margin-top:30px;">
-            <thead>
-                <tr>
-                    <th>Caixa</th>
-                    <th>Forma de Pgamento</th>
-                    <th>Atendente</th>
-                    <th>Valor</th>
-                </tr>
-            </thead>
-            <tbody>
-        <?php
-        $q = "select a.*, b.nome as atendente_nome from vendas_pagamento a left join atendentes b on a.atendente = b.codigo where a.venda = '{$d->codigo}' and a.deletado != '1'";
-        $r = mysqli_query($con, $q);
-        while($p = mysqli_fetch_object($r)){
-        ?>
-                <tr>
-                    <td><?=$p->caixa?></td>
-                    <td><?=$p->forma_pagamento?></td>
-                    <td><?=$p->atendente_nome?></td>
-                    <td>R$ <?=number_format($p->valor,2,'.',false)?></td>
-                </tr>    
-        <?php
-        }
-        ?>
-            </tbody>
-        </table>
+        <div style="background-color:#fff">
+            <table class="table" style="margin-top:30px;">
+                <thead>
+                    <tr>
+                        <th>Caixa</th>
+                        <th>Forma de Pgamento</th>
+                        <th>Atendente</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+            <?php
+            $q = "select a.*, b.nome as atendente_nome from vendas_pagamento a left join atendentes b on a.atendente = b.codigo where a.venda = '{$d->codigo}' and a.deletado != '1'";
+            $r = mysqli_query($con, $q);
+            while($p = mysqli_fetch_object($r)){
+            ?>
+                    <tr>
+                        <td><?=$p->caixa?></td>
+                        <td><?=$p->forma_pagamento?></td>
+                        <td><?=$p->atendente_nome?></td>
+                        <td>R$ <?=number_format($p->valor,2,'.',false)?></td>
+                    </tr>    
+            <?php
+            }
+            ?>
+                </tbody>
+            </table>
+        </div>
 
 
     </p>
